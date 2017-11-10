d9694f7abb00f2d2b5215cf1bdafdb4beec903a0
Leo Germani <leogermani@hacklab.com.br>
2017-11-10 19:08:12 -0200

    configuração de pasta de testes fora do git

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,9 +4,11 @@
  *
  * @package Test_Tainacan
  */
+$bootstrap_cfg = require('bootstrap-config.php');
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
-    $_tests_dir = '/tmp/wordpress-tests-lib';
+    $_tests_dir = $bootstrap_cfg['tests_dir'];
 }
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
