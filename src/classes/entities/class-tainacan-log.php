b18962df06cca614fc8000d2dca032e291b1b650
weryques <weryques@outlook.com>
2017-11-17 12:10:35 -0200

    PHPDoc log

--- src/classes/entities/class-tainacan-log.php
+++ src/classes/entities/class-tainacan-log.php
@@ -2,10 +2,11 @@
 
 namespace Tainacan\Entities;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+/**
+ * Representa a entidade Log
+ */
 class Log extends \Tainacan\Entity {
     
     function __construct($which = 0) {
@@ -26,45 +27,97 @@ class Log extends \Tainacan\Entity {
         
     }
 
-    // Getters
-    //
+    /**
+     * Retorna ID do Log
+     *
+     * @return integer
+     */
     function get_id() {
         return $this->get_mapped_property('id');
     }
+
+    /**
+     * Retorna titulo do Log
+     *
+     * @return string
+     */
     function get_title() {
         return $this->get_mapped_property('title');
     }
+
+    /**
+     * Retorna tipo de ordenação do Log
+     *
+     * @return string
+     */
     function get_order() {
         return $this->get_mapped_property('order');
     }
+
+    /**
+     * Retorna ID do parent do Log
+     *
+     * @return integer
+     */
     function get_parent() {
         return $this->get_mapped_property('parent');
     }
+
+    /**
+     * Retorna descrição do Log
+     *
+     * @return string
+     */
     function get_description() {
         return $this->get_mapped_property('description');
     }
     
     /**
      * Return User Id of who make the action
+     * 
      * @return int User Id of logged action
      */
     function get_user_id() {
     	return $this->get_mapped_property('user_id');
     }
         
-    // Setters
-    // 
-    
+    /**
+     * Atribui titulo ao log
+     *
+     * @param [type] $value
+     * @return void
+     */
     function set_title($value) {
-        return $this->set_mapped_property('title', $value);
+        $this->set_mapped_property('title', $value);
     }
+
+    /**
+     * Atribui tipo de ordenação
+     *
+     * @param [string] $value
+     * @return void
+     */
     function set_order($value) {
-        return $this->set_mapped_property('order', $value);
+        $this->set_mapped_property('order', $value);
     }
+
+    /**
+     * Atribui ID do parent ao log
+     *
+     * @param [integer] $value
+     * @return void
+     */
     function set_parent($value) {
-        return $this->set_mapped_property('parent', $value);
+        $this->set_mapped_property('parent', $value);
     }
+
+    /**
+     * Atribui descrição ao log
+     *
+     * @param [string] $value
+     * @return void
+     */
     function set_description($value) {
-        return $this->set_mapped_property('description', $value);
+        $this->set_mapped_property('description', $value);
     }
 }
