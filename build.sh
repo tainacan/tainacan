4f195b396be668c28c988d96007f4e69b549e8c5
weryques <weryques@outlook.com>
2017-11-24 15:32:27 -0200

    Tainacan API and two Endpoints: collections and collections/id

--- build.sh
+++ build.sh
@@ -2,14 +2,13 @@
 
 #source build-config.cfg
 source build-config.cfg
-#destination=~/devel/wordpress/wp-content/plugins/test-tainacan
+#destination=~/devel/wordpress/wp-content/plugins/tainacan
 
 sh compile-sass.sh
 composer install
 
 echo "Atualizando arquivos em $destination"
-rm -r $destination
+rm -rf $destination
 mkdir $destination
 cp -R src/ $destination/
-cp -R vendor/ $destination/
 rm -rf $destination/scss
