f9d10e97c47d7d789cd93387c5b07b3bb347add4
Jacson Passold <jacson@ethymos.com.br>
2017-11-30 14:45:10 -0200

    remove default value for compatibility with item meta fetch

--- src/classes/repositories/class-tainacan-repository.php
+++ src/classes/repositories/class-tainacan-repository.php
@@ -112,7 +112,7 @@ abstract class Repository {
      * @param $args
      * @return mixed
      */
-    public abstract function fetch( $args = [], $output = null );
+    public abstract function fetch( $args , $output = null );
 
     /**
      * @param $object
