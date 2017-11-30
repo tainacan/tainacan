cd498f48a6f0b4c75a15785b8254f40404c54985
weryques <weryques@outlook.com>
2017-11-30 14:00:43 -0200

    API, Docs and Tests config:
    setup-local.md updated;
    Collections Controller updated;
    Items Controller implementation initiated;
    Map in Collections updated;
    PHPDoc in Collections Controller; and
    other things.

--- src/api/endpoints/class-tainacan-rest-collections-controller.php
+++ src/api/endpoints/class-tainacan-rest-collections-controller.php
@@ -3,11 +3,20 @@
 use Tainacan\Repositories;
 use Tainacan\Entities;
 
+/**
+ * Class that represents the Collections REST Controller
+ *
+ * @uses Entities\Collection and Repositories\Collections
+ * */
 class TAINACAN_REST_Collections_Controller extends WP_REST_Controller {
     private $collections_repository;
     private $collection;
 
-    public function __construct(){
+	/**
+	 * TAINACAN_REST_Collections_Controller constructor.
+	 * Define the namespace, rest base and instantiate your attributes.
+	 */
+	public function __construct(){
         $this->namespace = 'tainacan/v2';
         $this->rest_base = 'collections';
         $this->collections_repository = new Repositories\Collections();
@@ -16,26 +25,28 @@ class TAINACAN_REST_Collections_Controller extends WP_REST_Controller {
         add_action('rest_api_init', array($this, 'register_routes'));
     }
 
-    public function register_routes(){
+	/**
+	 * Register the collections route and their endpoints
+	 */
+	public function register_routes(){
         register_rest_route($this->namespace, '/' . $this->rest_base, array(
             array(
                 'methods'             => WP_REST_Server::READABLE,
                 'callback'            => array($this, 'get_items'),
                 'permission_callback' => array($this, 'get_items_permissions_check'),
-                'args'                => $this->get_collection_params(),
             ),
 	        array(
 		        'methods'             => WP_REST_Server::CREATABLE,
 		        'callback'            => array($this, 'create_item'),
-		        'permission_callback' => array($this, 'create_item_permissions_check'),
+		        //'permission_callback' => array($this, 'create_item_permissions_check'),
 		        'args'                => $this->get_endpoint_args_for_item_schema(WP_REST_Server::CREATABLE),
 	        ),
-            'schema' => array($this, 'get_public_item_schema'),
         ));
-        register_rest_route($this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)', array(
+        register_rest_route($this->namespace, '/' . $this->rest_base . '/(?P<collection_id>[\d]+)', array(
             array(
                 'methods'             => WP_REST_Server::READABLE,
                 'callback'            => array($this, 'get_item'),
+                'args'                => $this->get_collection_params(),
                 'permission_callback' => array($this, 'get_item_permissions_check'),
             ),
             array(
@@ -51,7 +62,14 @@ class TAINACAN_REST_Collections_Controller extends WP_REST_Controller {
         ));
     }
 
-    public function get_items($request){
+	/**
+	 * Return a array of Collections objects in JSON
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public function get_items($request){
         $collections = $this->collections_repository->fetch();
 
         $response = $this->prepare_item_for_response($collections, $request);
@@ -59,8 +77,15 @@ class TAINACAN_REST_Collections_Controller extends WP_REST_Controller {
         return new WP_REST_Response($response, 200);
     }
 
-    public function get_item($request){
-        $collection_id = $request['id'];
+	/**
+	 * Return a Collection object in JSON
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public function get_item($request){
+        $collection_id = $request['collection_id'];
         $collection = $this->collections_repository->fetch($collection_id);
 
         $response = $this->prepare_item_for_response($collection, $request);
@@ -68,13 +93,28 @@ class TAINACAN_REST_Collections_Controller extends WP_REST_Controller {
         return new WP_REST_Response($response, 200);
     }
 
-    public function prepare_item_for_response($item, $request){
-        if(is_array($item)){
-
+	/**
+	 *
+	 * Receive a WP_Query or a Collection object and return both in JSON
+	 *
+	 * @param mixed $item
+	 * @param WP_REST_Request $request
+	 *
+	 * @return mixed|string|void|WP_Error|WP_REST_Response
+	 */
+	public function prepare_item_for_response($item, $request){
+        if($item instanceof WP_Query){
             $collections_as_json = [];
-            foreach ($item as $wp_post){
-                array_push($collections_as_json, $wp_post->__toJSON());
-            }
+
+	        if ($item->have_posts()) {
+		        while ( $item->have_posts() ) {
+		        	$item->the_post();
+		        	$collection = new Entities\Collection($item->post);
+			        array_push($collections_as_json, $collection->__toJSON());
+
+		        }
+		        wp_reset_postdata();
+	        }
 
             return json_encode($collections_as_json);
         }
@@ -83,22 +123,40 @@ class TAINACAN_REST_Collections_Controller extends WP_REST_Controller {
         }
     }
 
-    public function get_item_schema(){
-
+	/**
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return bool|WP_Error
+	 */
+	public function get_items_permissions_check($request){
+		return true;
     }
 
-    public function get_items_permissions_check($request){
+	/**
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return bool|WP_Error
+	 */
+	public function  get_item_permissions_check($request){
         return true;
     }
 
-    public function  get_item_permissions_check($request){
-        return true;
-    }
-
-    public function create_item( $request ) {
+	/**
+	 * Receive a JSON with the structure of a Collection and return, in case of success insert
+	 * a Collection object in JSON
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return array|WP_Error|WP_REST_Response
+	 */
+	public function create_item( $request ) {
     	$request = json_decode($request->get_body(), true);
 
-        if (!empty($request['id'])){
+    	if(empty($request)) {
+			return new WP_Error('rest_empty', __('Empty request.'), array('status' => 204));
+	    } elseif (!empty($request['collection_id'])){
 	        return new WP_Error( 'rest_post_exists', __( 'Cannot create existing post.' ), array( 'status' => 400 ) );
         }
 
@@ -107,47 +165,95 @@ class TAINACAN_REST_Collections_Controller extends WP_REST_Controller {
         if($prepared_post->validate()) {
 	        $collection = $this->collections_repository->insert( $prepared_post );
 
-	        return new WP_REST_Response($collection->__toJSON(), 200);
+	        return new WP_REST_Response($collection->__toJSON(), 201);
         }
 
-        return $prepared_post->get_errors();
+        return new WP_REST_Response($prepared_post->get_errors(), 400);
     }
 
-    public function create_item_permissions_check( $request ) {
-	    //if(current_user_can('edit_posts')){
+	/**
+	 * Verify if current has permission to create a item
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return bool|WP_Error
+	 */
+	public function create_item_permissions_check( $request ) {
+	    if(current_user_can('edit_posts')){
             return true;
-        //}
+        }
+
+        return false;
     }
 
-    public function prepare_item_for_database( $request ) {
+	/**
+	 * Prepare collection for insertion on database
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return object|Entities\Collection|WP_Error
+	 */
+	public function prepare_item_for_database( $request ) {
         $this->collection->set_name($request['name']);
 	    $this->collection->set_description($request['description']);
 
         return $this->collection;
     }
 
+	/**
+	 * Delete a collection
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return string|WP_Error|WP_REST_Response
+	 */
 	public function delete_item( $request ) {
-	    return parent::delete_item( $request ); // TODO: Change the autogenerated stub
+	    $collection_id = $request['collection_id'];
+	    $this->collections_repository->delete($collection_id);
+
+	    return 'Não implementado';
     }
 
-    public function delete_item_permissions_check( $request ) {
+	/**
+	 * Verify if current user has permission to delete a collection
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return bool|WP_Error
+	 */
+	public function delete_item_permissions_check( $request ) {
 	    if(current_user_can('delete_posts')){
 		    return true;
 	    }
+
+	    return false;
     }
 
-    public function update_item( $request ) {
-	    return parent::update_item( $request ); // TODO: Change the autogenerated stub
+	/**
+	 * Update a collection
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return string|WP_Error|WP_REST_Response
+	 */
+	public function update_item( $request ) {
+	    return 'Não implementado';
     }
 
-    public function update_item_permissions_check( $request ) {
+
+	/**
+	 * Verify if current user has permission to update a item
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return bool|WP_Error
+	 */
+	public function update_item_permissions_check( $request ) {
 	    if(current_user_can('edit_posts')){
 		    return true;
 	    }
-    }
 
-    public function get_public_item_schema() {
-	    return parent::get_public_item_schema(); // TODO: Change the autogenerated stub
+	    return false;
     }
 
     public function get_collection_params() {
@@ -157,7 +263,14 @@ class TAINACAN_REST_Collections_Controller extends WP_REST_Controller {
     }
 
     public function get_endpoint_args_for_item_schema( $method = WP_REST_Server::CREATABLE ) {
-	    return parent::get_endpoint_args_for_item_schema( $method ); // TODO: Change the autogenerated stub
+	    $args = [
+	    	'Object' => [
+	    		'type'        => 'JSON',
+			    'description' => 'A Collection object'
+		    ]
+	    ];
+
+	    return apply_filters("rest_{$this->collection->get_post_type()}_collection_params", $args, $this->collection->get_post_type());
     }
 }
 
--- src/api/tainacan-rest-creator.php
+++ src/api/tainacan-rest-creator.php
@@ -5,6 +5,10 @@ const ENDPOINTS_DIR = __DIR__ . '/endpoints/';
 require_once(ENDPOINTS_DIR . 'class-tainacan-rest-collections-controller.php');
 $rest_collections_controller = new TAINACAN_REST_Collections_Controller();
 
+require_once(ENDPOINTS_DIR . 'class-tainacan-rest-items-controller.php');
+$rest_items_controller = new TAINACAN_REST_Items_Controller();
+
+
 // Add here other endpoints imports
 
 ?>
