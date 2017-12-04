9ff4438236893c28b848ee5e96bf210585b365dd
Leo Germani <leogermani@hacklab.com.br>
2017-12-04 16:34:47 -0200

    only run npm build when js changes

--- build.sh
+++ build.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-#source build-config.cfg
 source build-config.cfg
-#destination=~/devel/wordpress/wp-content/plugins/tainacan
 
+## Only run npm build if there was a change in a .js or .vue file
+current_md5=$(<last-js-build.md5)
+find src -type f \( -name "*.js" -or -name "*.vue" \) -exec md5sum {} \; | sort -k 2 | md5sum > last-js-build.md5
+new_md5=$(<last-js-build.md5)
+if [ "$current_md5" != "$new_md5" ]
+then
+    npm run build
+fi
+### END npm build ###
+
+## Compile SASS
 sh compile-sass.sh
-npm run build
 
+## Install composer dependencies
 composer install
 
-echo "Atualizando arquivos em $destination"
+echo "Updating files in $destination"
 rm -rf $destination
 mkdir $destination
 cp -R src/* $destination/
