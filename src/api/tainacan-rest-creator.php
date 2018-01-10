0541d78b5fb1ead74a0163d73d4505244700a278
weryques <weryques@outlook.com>
2018-01-10 13:56:55 -0200

    Create endpoint to POST filters and your test.

--- src/api/tainacan-rest-creator.php
+++ src/api/tainacan-rest-creator.php
@@ -5,6 +5,7 @@ $rest_items_controller       = new TAINACAN_REST_Items_Controller();
 $rest_metadata_controller    = new TAINACAN_REST_Metadata_Controller();
 $rest_taxonomies_controller  = new TAINACAN_REST_Taxonomies_Controller();
 $rest_terms_controller       = new TAINACAN_REST_Terms_Controller();
+$rest_filters_controller     = new TAINACAN_REST_Filters_Controller();
 // Add here other endpoints imports
 
 ?>
