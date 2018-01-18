221adb35e6cdfd2e2fa6059f0c081042a9a52930
Jacson Passold <jacson@ethymos.com.br>
2018-01-18 12:28:20 -0200

    add some documentation

--- src/classes/traits/class-tainacan-entity-collection-relation.php
+++ src/classes/traits/class-tainacan-entity-collection-relation.php
@@ -2,16 +2,30 @@
 
 namespace Tainacan\Traits;
 use Tainacan\Entities;
+use Tainacan\Entities\Collection;
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 // used by Item, Event, Field
 
+/**
+ * Defines Collection and Items relation
+ * @author medialab
+ *
+ */
 trait Entity_Collection_Relation {
 
+	/**
+	 * 
+	 * @return int collection item ID
+	 */
     public function get_collection_id() {
         return $this->get_mapped_property('collection_id');
     }
     
+    /**
+     * Return Collection from relation
+     * @return \Entities\Collection|NULL Return Collection or null on errors
+     */
     public function get_collection() {
     	if (isset($this->collection) && $this->collection instanceof Entities\Collection)
             return $this->collection;
@@ -26,11 +40,19 @@ trait Entity_Collection_Relation {
         return null;
     }
     
+    /**
+     * Set collection ID
+     * @param int $value
+     */
     public function set_collection_id($value) {
         $this->collection = null;
         $this->set_mapped_property('collection_id', $value);  
     }
     
+    /**
+     * set collection object and id
+     * @param Entities\Collection $collection
+     */
     public function set_collection(Entities\Collection $collection) {
         $this->collection = $collection;
         $this->set_collection_id($collection->get_id());
