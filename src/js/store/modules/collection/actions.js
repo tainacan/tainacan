97bab80b5290d554741a5abd4884a589e25a3c59
mateuswetah <mateus.m.luna@gmail.com>
2018-02-21 09:59:45 -0300

    Minor corrections on tables display.

--- src/js/store/modules/collection/actions.js
+++ src/js/store/modules/collection/actions.js
@@ -32,7 +32,7 @@ export const fetchCollections = ({commit} , { page, collectionsPerPage }) => {
             let collections = res.data;
             commit('setCollections', collections);
             resolve({'collections': collections, 'total': res.headers['x-wp-total'] });
-        })
+        }) 
         .catch(error => {
             console.log(error);
             reject(error);
