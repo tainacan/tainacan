a737c9e9118d2fed26c648dc9576954843dfde2f
mateuswetah <mateus.m.luna@gmail.com>
2018-02-21 15:38:51 -0300

    Adds basic routes and empty pages components. Removes Create button from CollectionsList, passes do CollectionsPage.

--- src/admin/admin.vue
+++ src/admin/admin.vue
@@ -1,24 +1,23 @@
 <template>
-    <div id="tainacan-admin-app">
-        <div class="columns is-fullheight">
-            <nav id="primary-menu" role="navigation" aria-label="main navigation" class="column is-2 is-sidebar-menu is-hidden-mobile">
-                <aside class="menu">
-                    <router-link to="/">
-                        <img :src="logoHeader" alt="Tainacan Admin" height="32">
-                    </router-link>
-                    <ul class="menu-list">
-                        <li><router-link tag="a" to="/collections">{{ $i18n.getString('header', 'collections')}}</router-link></li>
-                        <li><a class="navbar-item">Items</a></li>
-                        <li><a class="navbar-item">Campos</a></li>
-                        <li><a class="navbar-item">Filtros</a></li>
-                        <li><a class="navbar-item">Atividades</a></li>
-                        <li><a class="navbar-item" :href="wordpressAdmin">Admin do Wordpress</a></li>
-                    </ul>
-                </aside>
-            </nav>
-            <div class="container column is-main-content">
-                <router-view></router-view>
-            </div>
+    <div id="tainacan-admin-app" class="columns is-fullheight">
+        <nav id="primary-menu" role="navigation" aria-label="main navigation" class="column is-2 is-sidebar-menu">
+            <aside class="menu">
+                <router-link to="/">
+                    <img :src="logoHeader" alt="Tainacan Admin" height="32">
+                </router-link>
+                <ul class="menu-list">
+                    <li><router-link tag="a" to="/collections">{{ $i18n.getString('menu', 'collections')}}</router-link></li>
+                    <li><router-link tag="a" to="/items">{{ $i18n.getString('menu', 'items')}}</router-link></li>
+                    <li><router-link tag="a" to="/fields">{{ $i18n.getString('menu', 'fields')}}</router-link></li>
+                    <li><router-link tag="a" to="/filters">{{ $i18n.getString('menu', 'filters')}}</router-link></li>
+                    <li><router-link tag="a" to="/categories">{{ $i18n.getString('menu', 'categories')}}</router-link></li>
+                    <li><router-link tag="a" to="/events">{{ $i18n.getString('menu', 'events')}}</router-link></li>
+                    <li><a class="navbar-item" :href="wordpressAdmin">Admin do Wordpress</a></li>
+                </ul>
+            </aside>
+        </nav>
+        <div class="container column is-main-content">
+            <router-view></router-view>
         </div>
     </div>
 </template>
@@ -39,8 +38,13 @@
 
     @import "./scss/_variables.scss";
 
+    .is-fullheight {
+        height: 100%;
+    }
+
     #primary-menu {
         background-color: $primary;
+
         li{
             a {color: white !important;}
             a:hover {color: $primary !important;}
---
+++ src/admin/pages/fields-page.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Fields Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FieldsPage',
+    data(){
+        return {
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
