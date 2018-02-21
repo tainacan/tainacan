121a481f6cd8cd3db7f1f5f73c9a20a762646007
mateuswetah <mateus.m.luna@gmail.com>
2018-02-21 12:48:42 -0300

    Initialize refactor of nav bar do a sidemenu.

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
             }
         ]
     },
