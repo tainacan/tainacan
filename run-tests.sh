78ede584c04ffe7decb1f2e390a6642159d0946c
Leo Germani <leogermani@hacklab.com.br>
2018-03-02 18:03:34 -0300

    better way to tear down wordpress for cypress tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 source build-config.cfg
 
 #./build.sh 
@@ -9,45 +8,34 @@ plugin_name=`basename $destination`
 test_db_prefix='wp_tainacan_test_'
 
 
-# set up cypress environment
+################## set up cypress environment
 # replace table prefix in wp_config
+echo "Changing DB prefix in wp_config..."
 sed -i s/"require_once(ABSPATH . 'wp-settings.php');"/"\$table_prefix  = '$test_db_prefix';\nrequire_once(ABSPATH . 'wp-settings.php');"/ $wp_base_dir/wp-config.php 
 
 # install WordPress
 cd $wp_base_dir
+echo "Installing WordPress..."
 wp core install --url=$wp_url --title=Test --admin_user=admin --admin_password=admin --admin_email=admin@admin.com
 
 # Activate Tainacan
 wp plugin activate $plugin_name
 
-
+# back to tainacan dev directory
+cd -
 #npx cypress run --env host=$wp_url,baseUrl=$wp_url
 
 
 
-# teardown cypress environment
+################## teardown cypress environment
 
 # remove WordPress
+echo "Removing WordPress..."
+cd $wp_base_dir
+wp db query "DROP TABLES $(wp db tables | paste -s -d, -);"
 
 # recover table prefix in wp_config
+echo "Restoring DB prefix in wp_config..."
 sed -i s/"\$table_prefix  = '$test_db_prefix';"/""/ $wp_base_dir/wp-config.php 
 
-TEST_DB_NAME=`cat $wp_base_dir/wp-config.php | grep "define('DB_NAME'"| perl -pe 's|define\(.DB_NAME.,\s*.(.*?)\W\s*\).*|$1|'`
-TEST_DB_USER=`cat $wp_base_dir/wp-config.php | grep "define('DB_USER'"| perl -pe 's|define\(.DB_USER.,\s*.(.*?)\W\s*\).*|$1|'`
-TEST_DB_PASS=`cat $wp_base_dir/wp-config.php | grep "define('DB_PASSWORD'"| perl -pe 's|define\(.DB_PASSWORD.,\s*.(.*?)\W\s*\).*|$1|'`
-TEST_DB_HOST=`cat $wp_base_dir/wp-config.php | grep "define('DB_HOST'"| perl -pe 's|define\(.DB_HOST.,\s*.(.*?)\W\s*\).*|$1|'`
-
-
-echo DROP TABLE $test_db_prefix'comments' | mysql -u $TEST_DB_USER -p$TEST_DB_PASS -h $TEST_DB_HOST $TEST_DB_NAME
-echo DROP TABLE $test_db_prefix'commentmeta' | mysql -u $TEST_DB_USER -p$TEST_DB_PASS -h $TEST_DB_HOST $TEST_DB_NAME
-echo DROP TABLE $test_db_prefix'links' | mysql -u $TEST_DB_USER -p$TEST_DB_PASS -h $TEST_DB_HOST $TEST_DB_NAME
-echo DROP TABLE $test_db_prefix'options' | mysql -u $TEST_DB_USER -p$TEST_DB_PASS -h $TEST_DB_HOST $TEST_DB_NAME
-echo DROP TABLE $test_db_prefix'postmeta' | mysql -u $TEST_DB_USER -p$TEST_DB_PASS -h $TEST_DB_HOST $TEST_DB_NAME
-echo DROP TABLE $test_db_prefix'posts' | mysql -u $TEST_DB_USER -p$TEST_DB_PASS -h $TEST_DB_HOST $TEST_DB_NAME
-echo DROP TABLE $test_db_prefix'term_relationships' | mysql -u $TEST_DB_USER -p$TEST_DB_PASS -h $TEST_DB_HOST $TEST_DB_NAME
-echo DROP TABLE $test_db_prefix'term_taxonomy' | mysql -u $TEST_DB_USER -p$TEST_DB_PASS -h $TEST_DB_HOST $TEST_DB_NAME
-echo DROP TABLE $test_db_prefix'termmeta' | mysql -u $TEST_DB_USER -p$TEST_DB_PASS -h $TEST_DB_HOST $TEST_DB_NAME
-echo DROP TABLE $test_db_prefix'terms' | mysql -u $TEST_DB_USER -p$TEST_DB_PASS -h $TEST_DB_HOST $TEST_DB_NAME
-echo DROP TABLE $test_db_prefix'usermeta' | mysql -u $TEST_DB_USER -p$TEST_DB_PASS -h $TEST_DB_HOST $TEST_DB_NAME
-echo DROP TABLE $test_db_prefix'users' | mysql -u $TEST_DB_USER -p$TEST_DB_PASS -h $TEST_DB_HOST $TEST_DB_NAME
 
