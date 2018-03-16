61021b33a6c696bc1cc2b3b70eaf4d9e9946c490
Jacson Passold <jacson@ethymos.com.br>
2018-03-16 15:45:18 -0300

    register exposers types and mappers

--- src/exposers/class-tainacan-exposers.php
+++ src/exposers/class-tainacan-exposers.php
@@ -12,6 +12,9 @@ class Exposers {
 	private $mappers = [];
 	
 	public function __construct() {
+		$this->register_exposer_type('Tainacan\Exposers\Types\Xml');
+		$this->register_exposer_mapper('Tainacan\Exposers\Mappers\Dublin_Core');
+		
 		add_filter( 'rest_request_after_callbacks', [$this, 'rest_request_after_callbacks'], 10, 3 );
 		add_filter( 'tainacan-rest-response', [$this, 'rest_response'], 10, 2 );
 	}
@@ -31,9 +34,24 @@ class Exposers {
 		}
 	}
 	
+	/**
+	 * register exposers mappers class on array of types
+	 *
+	 * @param $class_name string | object The class name or the object instance
+	 */
+	public function register_exposer_mapper( $class_name ){
+		if( is_object( $class_name ) ){
+			$class_name = get_class( $class_name );
+		}
+		
+		if(!in_array( $class_name, $this->mappers)){
+			$this->mappers[] = $class_name;
+		}
+	}
+	
 	protected function check_class_name($class_name, $root = false, $prefix = 'Tainacan\Exposers\Types\\') {
 		$class = $prefix.sanitize_text_field($class_name);
-		$class = str_replace(['-', ' '], ['_'], $class);
+		$class = str_replace(['-', ' '], ['_', '_'], $class);
 		
 		return ($root ? '\\' : '').$class;
 	}
@@ -42,13 +60,14 @@ class Exposers {
 		if($request->get_method() == 'GET' && substr($request->get_route(), 0, strlen('/tainacan/v2')) == '/tainacan/v2') {
 			$body = json_decode( $request->get_body(), true );
 			$class_prefix = 'Tainacan\Exposers\Mappers\\';
+			
 			if(
 				is_array($body) && array_key_exists('exposer-map', $body) &&
-					in_array($this->check_class_name($body['exposer-map'], false, $class_prefix), $this->mappers)
+				in_array($this->check_class_name($body['exposer-map'], false, $class_prefix), $this->mappers)
 			) {
 				$type = $this->check_class_name($body['exposer-map'], true, $class_prefix);
 				$exposer = new $type;
-				return $exposer->rest_response($response, $handler, $request);
+				return $exposer->rest_response($item_arr, $request);
 			}
 		}
 		return $item_arr;
