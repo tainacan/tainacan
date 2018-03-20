c60902a035cc5830e5a5aa5c93198a3df94eec2c
weryques <weryques@outlook.com>
2018-03-20 14:14:27 -0300

    Build watch for macOS

--- build-watch.sh
+++ build-watch.sh
@@ -4,8 +4,16 @@
 
 echo 'Watching changes on src/'
 
-while inotifywait -qqr src -e create,move,modify,delete; do
+current_OS=`uname`
+
+# For macOS (Darwin) is needed fsevents-tools installed (you can use homebrew install fsevents-tools)
+if [ $current_OS == "Darwin" ]; then
     echo
-    echo 'Change detected, running build'
-    ./build.sh
-done
+    notifyloop src ./build.sh
+else
+    while inotifywait -qqr src -e create,move,modify,delete; do
+        echo
+        echo 'Change detected, running build'
+        ./build.sh
+    done
+fi
