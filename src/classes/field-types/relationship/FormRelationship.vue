092dda4a9c08d874b56bdb30a0902d02d98ee361
eduardohumberto <eduardo.humberto1992@gmail.com>
2018-03-21 12:31:59 -0300

    add label on checkboxes in field types forms

--- src/classes/field-types/relationship/FormRelationship.vue
+++ src/classes/field-types/relationship/FormRelationship.vue
@@ -55,15 +55,17 @@
 
 
         <b-field :addons="false">
-
+            <label class="label">
+                {{ $i18n.get('label_allow_repeated_items') }}
+                <a class="help-button"><b-icon size="is-small" icon="help-circle-outline"></b-icon></a>
+            </label>
             <div class="block">
                 <b-checkbox v-model="modelRepeated"
                           @input="emitValues()"
                           true-value="yes"
                           false-value="no">
-                        {{ $i18n.get('label_allow_repeated_items') }}
+                        {{ labelRepeated() }}
                 </b-checkbox>
-                <a class="help-button"><b-icon size="is-small" icon="help-circle-outline"></b-icon></a>
             </div>
         </b-field>
     </section>
