c9695b537ffd67cf5eef4f2274c23b508e089357
weryques <weryques@outlook.com>
2018-03-23 16:04:05 -0300

    Better mixin

--- src/admin/js/mixins.js
+++ src/admin/js/mixins.js
@@ -2,14 +2,20 @@ import axios from 'axios';
 import qs from  'qs';
 
 export const wpAjax = {
+    data(){
+      return {
+          axiosWPAjax: {},
+      }
+    },
+    created(){
+        this.axiosWPAjax = axios.create({
+            baseURL: tainacan_plugin.wp_ajax_url,
+        });
+    },
     methods: {
         getSamplePermalink(id, newTitle, newSlug){
-            let axiosWPAjax = axios.create({
-                baseURL: tainacan_plugin.wp_ajax_url,
-            });
-
             return new Promise((resolve, reject) => {
-                axiosWPAjax.post('', qs.stringify({
+                this.axiosWPAjax.post('', qs.stringify({
                     action: 'sample-permalink',
                     post_id: id,
                     new_title: newTitle,
