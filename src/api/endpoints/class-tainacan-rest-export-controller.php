d420e815837d5228936a429e25d3ab8127727891
Jacson Passold <jacson@ethymos.com.br>
2018-05-15 18:23:24 -0300

    add export api

--- src/api/endpoints/class-tainacan-rest-export-controller.php
+++ src/api/endpoints/class-tainacan-rest-export-controller.php
@@ -5,6 +5,7 @@ namespace Tainacan\API\EndPoints;
 use \Tainacan\API\REST_Controller;
 use Tainacan\Entities;
 use Tainacan\Repositories;
+use Tainacan\Entities\Entity;
 
 class REST_Export_Controller extends REST_Controller {
 	private $item_metadata_repository;
@@ -126,7 +127,7 @@ class REST_Export_Controller extends REST_Controller {
 	}
 
 	/**
-	 * @param \Tainacan\Entities\Entity $item
+	 * @param \Tainacan\Entities\Item $item
 	 * @param \WP_REST_Request $request
 	 *
 	 * @return array|\WP_Error|\WP_REST_Response
@@ -142,6 +143,118 @@ class REST_Export_Controller extends REST_Controller {
 
 		return $prepared_item;
 	}
+	
+	/**
+	 * 
+	 * @param \WP_REST_Request $request
+	 * @param \WP_Query|Entities\Item $query
+	 * @param array $args
+	 * @return \WP_Error|number
+	 */
+	public function export($request, $query, $args) {
+		
+		$type = \Tainacan\Exposers\Exposers::request_has_type($request);
+		$path = wp_upload_dir();
+		$path = $path['path'];
+		$filename = $path.date('YmdHis').'-tainacan-export.'.$type->get_extension();
+		$pid = -1;
+
+		$log = \Tainacan\Entities\Log::create(
+			__('Export Process', 'tainacan'),
+			__('Exporting Data', 'tainacan').'\nArgs: '.print_r($args, true),
+			['file' => $filename],
+			[],
+			'processing'
+		);
+		
+		$body = json_decode( $request->get_body(), true );
+		$background = ! (isset($body['export-background']) && $body['export-background'] == false);
+		if( $background ) {
+			$pid = pcntl_fork();
+		} else {
+			$pid = true;
+		}
+		if ($pid === -1) {
+			$error = new \WP_Error('could not fork');
+			$log = \Tainacan\Entities\Log::create(
+				__('Export Process Error', 'tainacan'),
+				__('Exporting Error', 'tainacan').'\\nArgs: '.print_r($args, true).'\\nError: could not fork',
+				$error,
+				[],
+				'error'
+			);
+			remove_filter( 'rest_request_after_callbacks', [\Tainacan\Exposers\Exposers::get_instance(), 'rest_request_after_callbacks'], 10, 3 ); //exposer mapping
+			remove_filter( 'tainacan-rest-response', [\Tainacan\Exposers\Exposers::get_instance(), 'rest_response'], 10, 2 ); // exposer types
+			return $log;
+		} elseif ($pid) { // we are the parent or run at foreground
+			try {
+				ignore_user_abort(true);
+				set_time_limit(0);
+				ini_set("memory_limit", "256M");
+				
+				if($background) { // wait for child to respond and exit and reconnect database if is forked
+					$status = null;
+					pcntl_wait($status);
+					global $wpdb;
+					$wpdb->db_connect();
+				}
+				
+				$response = [];
+				if(isset($request['collection_id'])) { // One Colletion
+					$collection_id = $request['collection_id'];
+					$items = $query;
+					if ($items->have_posts()) {
+						while ( $items->have_posts() ) { //TODO write line by line
+							$items->the_post();
+							
+							$item = new Entities\Item($items->post);
+							
+							$prepared_item = $this->prepare_item_for_response($item, $request);
+							
+							array_push($response, $prepared_item);
+							file_put_contents('/tmp/2', print_r($prepared_item, true), FILE_APPEND);
+						}
+						wp_reset_postdata();
+					}
+				} elseif (isset($request['item_id'])) { // One Item
+					
+					$item = new Entities\Item($request['item_id']);
+					if($item->get_id() > 0) {
+						$prepared_item = $this->prepare_item_for_response($item, $request);
+						
+						$response = [$prepared_item];
+					}
+				} else { // Export All
+					
+				}
+				
+				$rest_response = new \WP_REST_Response(apply_filters('tainacan-rest-response', $response, $request));
+				//file_put_contents($filename, $rest_response->get_data());
+				file_put_contents('/tmp/1', print_r($rest_response->get_data(), true));
+				
+				if($background) {
+					$log->set_status('publish');
+					$logs = \Tainacan\Repositories\Logs::get_instance();
+					$logs->update($log);
+					exit(1);
+				} else {
+					return $rest_response->get_data();
+				}
+			} catch (\Exception $e) {
+				if($background) {
+					exit(1);
+				} else {
+					throw $e;
+				}
+			}
+		} else { // we are the child
+
+			remove_filter( 'rest_request_after_callbacks', [\Tainacan\Exposers\Exposers::get_instance(), 'rest_request_after_callbacks'], 10, 3 ); //exposer mapping
+			remove_filter( 'tainacan-rest-response', [\Tainacan\Exposers\Exposers::get_instance(), 'rest_response'], 10, 2 ); // exposer types
+			return $log;
+		}
+		
+	}
 
 	/**
 	 * @param \WP_REST_Request $request
@@ -149,36 +262,38 @@ class REST_Export_Controller extends REST_Controller {
 	 * @return \WP_Error|\WP_REST_Response
 	 */
 	public function get_items( $request ) {
-		$args = $this->prepare_filters($request);
+		$args = $this->prepare_filters($request); // TODO default args
 		$rest_response = new \WP_REST_Response([], 200); // TODO error, empty response
 		
-		if(isset($request['collection_id'])) {
+		if(isset($request['collection_id'])) { // One Colletion
 			$collection_id = $request['collection_id'];
 			$items = $this->items_repository->fetch($args, $collection_id, 'WP_Query');
 			
-			$response = [];
-			if ($items->have_posts()) {
-				while ( $items->have_posts() ) {
-					$items->the_post();
-					
-					$item = new Entities\Item($items->post);
-					
-					$prepared_item = $this->prepare_item_for_response($item, $request);
-					
-					array_push($response, $prepared_item);
-				}
-				
-				wp_reset_postdata();
-			}
+			$response = $this->export($request, $items, $args);
 			
 			$total_items  = $items->found_posts;
-			$max_pages = ceil($total_items / (int) $items->query_vars['posts_per_page']);
-			
-			$rest_response = new \WP_REST_Response(apply_filters('tainacan-rest-response', $response, $request), 200);
+			$ret = $response instanceof Entity ? $response->__toArray() : $response;
+			$rest_response = new \WP_REST_Response($ret, 200);
 			
 			$rest_response->header('X-WP-Total', (int) $total_items);
-			$rest_response->header('X-WP-TotalPages', (int) $max_pages);
+		} elseif (isset($request['item_id'])) { // One Item
+					
+			$item = new Entities\Item($request['item_id']);
+			if($item->get_id() > 0) {
+				$response = $this->export($request, $item, $args);
+				
+				$total_items  = 1;
+				$max_pages = 1;
+				
+				$rest_response = new \WP_REST_Response($response->__toArray(), 200);
+				
+				$rest_response->header('X-WP-Total', 1);
+				$rest_response->header('X-WP-TotalPages', 1);
+			}
+		} else { // Export All
+			
 		}
+		
 		return $rest_response;
 	}
 
