cec067b9f53f6f5235e3fed3ff8182c6770168a8
walisonjose <walison.josededeus@gmail.com>
2018-06-04 14:09:36 -0300

    Update and rename bootstrap-config.php to bootstrap-config-sample.php

---
+++ tests/bootstrap-config-sample.php
@@ -0,0 +1,8 @@
+<?php 
+
+return [
+    'tests_dir' => '~/wordpress/wodpress-tests-lib',
+];
+
+
+ ?>
--- tests/bootstrap-config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-return [
-    'tests_dir' => '/home/travis/build/wordpress/wodpress-tests-lib',
-];
-
-
- ?>
