ae6bf8ff883d4681b69a18ba1f80c6250da58be6
Leo Germani <leogermani@hacklab.com.br>
2018-08-02 21:52:51 -0300

    add query time to get_items API response

--- src/api/endpoints/class-tainacan-rest-items-controller.php
+++ src/api/endpoints/class-tainacan-rest-items-controller.php
@@ -216,8 +216,12 @@ class REST_Items_Controller extends REST_Controller {
 		if($request['collection_id']) {
 			$collection_id = $request['collection_id'];
 		}
-
+		
+		$query_start = microtime(true);
+		
 		$items = $this->items_repository->fetch($args, $collection_id, 'WP_Query');
+		
+		$query_end = microtime(true);
 		
 		$response = [];
 
@@ -274,6 +278,8 @@ class REST_Items_Controller extends REST_Controller {
 
 		$rest_response->header('X-WP-Total', (int) $total_items);
 		$rest_response->header('X-WP-TotalPages', (int) $max_pages);
+		$rest_response->header('X-Tainacan-Query-Time', $query_end - $query_start);
+		$rest_response->header('X-Tainacan-Elapsed-Time', microtime(true) - $query_start);
 
 		return $rest_response;
 	}
