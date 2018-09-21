4000a6a27cf87e9853a15247f55a7768a1a49e58
weryques <weryques@outlook.com>
2018-09-21 14:51:06 -0300

    Rename method to update_tainacan_selectbox_to_tainacan_radio_and_tainacan_taginput

--- src/migrations.php
+++ src/migrations.php
@@ -241,7 +241,7 @@ class Migrations {
 	        '%s', '%s');
 	}
 
-	static function migrate_selectbox_to_radio_and_taginput(){
+	static function update_tainacan_selectbox_to_radio_and_taginput(){
 		global $wpdb;
 
 		// update filter type
