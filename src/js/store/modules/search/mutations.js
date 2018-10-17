c7f9f4f5492d6430e5224a8730fe2f054aa734ba
weryques <weryques@outlook.com>
2018-10-17 10:15:05 -0300

    Fixes conflict with advanced search and simple search
    Removes simple search when doing advanced search
    Refactoring removePostQuery method

--- src/js/store/modules/search/mutations.js
+++ src/js/store/modules/search/mutations.js
@@ -4,8 +4,8 @@ export const setPostQueryAttribute = ( state, { attr, value }) => {
     Vue.set( state.postquery, attr , value );
 };
 
-export const cleanPostQueryAttribute = ( state, { attr }) => {
-    Vue.set( state.postquery, attr , null );
+export const removePostQueryAttribute = ( state, attr) => {
+    delete state.postquery[`${attr}`];
 };
 
 export const setPostQuery = ( state, postquery ) => {
@@ -30,7 +30,7 @@ export const addMetaQuery = ( state, filter ) => {
             compare: filter.compare,
             type: filter.type
         } );
-    } else{
+    } else {
         state.postquery.metaquery.push({
             key: filter.metadatum_id,
             value: filter.value,
@@ -42,14 +42,16 @@ export const addMetaQuery = ( state, filter ) => {
 
 export const addTaxQuery = ( state, filter ) => {
     state.postquery.taxquery = ( ! state.postquery.taxquery || state.postquery.taxquery.length == undefined ) ? [] : state.postquery.taxquery;
+
     let index = state.postquery.taxquery.findIndex( item => item.taxonomy === filter.taxonomy);
+
     if ( index >= 0 ){
         Vue.set( state.postquery.taxquery, index, {
             taxonomy: filter.taxonomy,
             terms: filter.terms,
             compare: filter.compare
         } );
-    }else{
+    } else {
         state.postquery.taxquery.push({
             taxonomy: filter.taxonomy,
             terms: filter.terms,
@@ -68,11 +70,13 @@ export const addFetchOnly = ( state, metadatum ) => {
 };
 export const addFetchOnlyMeta = ( state, metadatum ) => {
     state.postquery.fetch_only = ( ! state.postquery.fetch_only ) ? { '0': 'thumbnail', 'meta': [], '1': 'creation_date', '2': 'author_name' } : state.postquery.fetch_only;
-   // console.log(state.postquery.fetch_only);
+    // console.log(state.postquery.fetch_only);
     //console.log(state.postquery.fetch_only['meta']);
     state.postquery.fetch_only['meta'] = ( ! state.postquery.fetch_only['meta'] ) ? [] : state.postquery.fetch_only['meta'];
+
     let index = state.postquery.fetch_only['meta'].findIndex( item => item == metadatum);
-    if ( index >= 0 ){    
+
+    if ( index >= 0 ){
         state.postquery.fetch_only['meta'][index] = metadatum;
     } else {
         state.postquery.fetch_only['meta'].push(metadatum);
@@ -90,6 +94,7 @@ export const removeFetchOnly = ( state, metadatum ) => {
 export const removeFetchOnlyMeta = ( state, metadatum ) => {
     if(state.postquery.fetch_only['meta'] != undefined) {
         let index = state.postquery.fetch_only['meta'].findIndex( item => item == metadatum);
+
         if (index >= 0) {
             state.postquery.fetch_only['meta'].splice(index, 1);
         }
@@ -98,7 +103,9 @@ export const removeFetchOnlyMeta = ( state, metadatum ) => {
 
 export const removeMetaQuery = ( state, filter ) => {
     state.postquery.metaquery = ( ! state.postquery.metaquery ) ? [] : state.postquery.metaquery;
+
     let index = state.postquery.metaquery.findIndex( item => item.key == filter.metadatum_id);
+
     if (index >= 0) {
         state.postquery.metaquery.splice(index, 1);
     }
@@ -106,13 +113,10 @@ export const removeMetaQuery = ( state, filter ) => {
 
 export const removeTaxQuery = ( state, filter ) => {
     let index = state.postquery.taxquery.findIndex( item => item.taxonomy == filter.taxonomy);
+
     if (index >= 0) {
         state.postquery.taxquery.splice(index, 1);
     }
-};
-
-export const removePostQueryAttribute = ( state, attribute) => {
-    Vue.set( state.postquery, attribute , '');  
 };
 
 export const setTotalItems = ( state, total ) => {
@@ -125,10 +129,11 @@ export const setTotalPages = ( state, totalPages ) => {
 
 export const setSearchQuery = ( state, searchQuery ) => {
     
-    if (searchQuery != '')
+    if (searchQuery != '') {
         state.postquery.search = searchQuery;
-    else    
-        state.postquery.search = undefined;
+    } else {
+        delete state.postquery.search;
+    }
 };
 
 export const setStatus = ( state, status ) => {
@@ -145,16 +150,21 @@ export const setAdminViewMode = ( state, adminViewMode ) => {
 
 export const addFilterTag = ( state, filterTag ) => {
     state.filter_tags = ( ! state.filter_tags) ? [] : state.filter_tags;
+
     let index = state.filter_tags.findIndex( tag => tag.filterId == filterTag.filterId);
-    if ( index >= 0 )
-        Vue.set( state.filter_tags, index, filterTag );
-    else
+
+    if ( index >= 0 ) {
+        Vue.set(state.filter_tags, index, filterTag);
+    } else {
         state.filter_tags.push(filterTag);
+    }
 };
 
 export const removeFilterTag = ( state, filterTag ) => {
     state.filter_tags = ( ! state.filter_tags ) ? [] : state.filter_tags;
+
     let index = state.filter_tags.findIndex( tag => tag.filterId == filterTag.filterId);
+
     if (index >= 0) {
         state.filter_tags.splice(index, 1);
     }
@@ -173,5 +183,5 @@ export const cleanTaxQueries = (state) => {
 };
 
 export const cleanFetchOnly = (state) => {
-    state.postquery.fetch_only = undefined;
+    delete state.postquery.fetch_only;
 };
