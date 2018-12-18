db119c53a85e82b7c0fa0a797d84c252b9d7bc18
vnmedeiros <vnicius.nm.ba@gmail.com>
2018-12-18 21:42:44 -0200

    fix regex to replace a links outsides a html tags and not (https,http..)

--- src/classes/traits/class-tainacan-formatter-text.php
+++ src/classes/traits/class-tainacan-formatter-text.php
@@ -16,8 +16,7 @@ trait Formatter_Text {
 	 * @return string Texts with url's transformed in html tag <a>
 	 */
 	public function make_clickable_links($text) {
-		//$url = '~(?:(http|https|ftp|ftps)://([^\s<]+)|(www\.[^\s<]+?\.[^\s<]+))(?<![\.,:])~i';
-		$url = '~((https?|ftps?):\/\/[^"<\s]+)(?![^<>]*>|[^"]*?<\/a)~i';
+		$url = '~((http:\/\/|https:\/\/|ftp:\/\/|ftps:\/\/)?www\.[^"<\s]+)(?![^<>]*>|[^"]*?<\/a)~i';
 		$text = preg_replace($url, '<a href="$0" target="_blank" title="$0">$0</a>', $text);
 		return $text;
 	}
