29634a4275187d08efe14bfdd4ff888a240cdef4
Mateus Machado Luna <mateus.m.luna@gmail.com>
2019-01-29 17:21:50 -0200

    Adds select to choose taxonomy and several improvements.

--- src/gutenberg-blocks/tainacan-terms/terms-list/index.js
+++ src/gutenberg-blocks/tainacan-terms/terms-list/index.js
@@ -2,7 +2,7 @@ const { registerBlockType } = wp.blocks;
 
 const { __ } = wp.i18n;
 
-const { Button, Modal, Autocomplete, TextareaControl, QueryControls, Placeholder, CheckboxControl } = wp.components;
+const { Autocomplete, SelectControl, Spinner, QueryControls, Placeholder } = wp.components;
 
 const { InspectorControls } = wp.editor;
 
@@ -13,11 +13,8 @@ registerBlockType('tainacan/terms-list', {
     title: __('Tainacan Terms List', 'tainacan'),
     icon: 'list-view',
     category: 'tainacan-blocks',
+    keywords: [ __( 'Tainacan', 'tainacan' ), __( 'terms', 'tainacan' ), __( 'taxonomy', 'tainacan' ) ],
     attributes: {
-        terms2: {
-          type: Array,
-          default: []
-        },
         terms: {
             type: 'array',
             source: 'query',
@@ -25,8 +22,7 @@ registerBlockType('tainacan/terms-list', {
             query: {
                 url: {
                     type: 'string',
-                    source: 'children',
-                    selector: 'a',
+                    source: 'attribute',
                     attribute: 'href'
                 },
                 name: {
@@ -35,10 +31,6 @@ registerBlockType('tainacan/terms-list', {
                 }, 
             },
             default: []
-        },
-        isOpen: {
-            type: Boolean,
-            default: false
         },
         content: {
             type: 'array',
@@ -57,20 +49,21 @@ registerBlockType('tainacan/terms-list', {
             type: String,
             default: ''
         },
-        tainacanURL: {
-            type: String,
-            default: ''
-        },
-        showItemsCount: {
+        isLoadingTaxonomies: {
             type: Boolean,
             default: false
         },
+        taxonomies: {
+            type: Array,
+            default: null
+        }
     },
     supports: {
         html: false
     },
     edit({ attributes, setAttributes, className, isSelected }){
-        let { terms, terms2, isOpen, content, termsPerPage, query, URLTaxonomyID, tainacanURL, showItemsCount } =  attributes;
+        let { terms, content, termsPerPage, query, URLTaxonomyID, isLoadingTaxonomies, taxonomies } =  attributes;
+        console.log("Editando...");
 
         function prepareTerm(term) {
             return (
@@ -82,7 +75,7 @@ registerBlockType('tainacan/terms-list', {
                             style={{
                                 // TODO
                             }}
-                            href={term.url} target="_blank">
+                            href={ term.url } target="_blank">
                         { term.name ? term.name : '' }
                     </a>
                 </li>
@@ -91,51 +84,28 @@ registerBlockType('tainacan/terms-list', {
 
         function getTerms(taxonomyID, query) {
             if(taxonomyID) {
-                return tainacan.get(
-                        `/taxonomy/${taxonomyID}/terms`
-                        // `/collection/${collectionID}/items?${query}`
-                    )
+                return tainacan.get(`/taxonomy/${taxonomyID}/terms?${query}`)
                     .then(response => {
                         return response.data;
                     })
                     .catch(error => {
                         console.error(error);
+                        return [];
                     });
             } else {
-                return tainacan.get(`/items?${query}`)
-                    .then(response => {
-                        return response.data;
-                    })
-                    .catch(error => {
-                        console.error(error);
-                    });
+                return [];
             }
         }
 
         function setContent(terms){
             setAttributes({
                 content: (
-                    <div style={{ margin: '12px' }}>
-                        <ul
-                                style={{
-                                    MozColumnCount: 4,
-                                    MozColumnGap: '7rem',
-                                    MozColumnRule: 'none',
-                                    WebkitColumnCount: 4,
-                                    WebkitColumnGap: '7rem',
-                                    WebkitColumnRule: 'none',
-                                    columnCount: 4,
-                                    columnGap: '7rem',
-                                    columnRule: 'none'
-                                }}>
-                            { terms }
-                        </ul>
-                    </div>
+                    <ul class="terms-list">{ terms }</ul>
                 )
             });
         }
         
-        function updateQuery(query) {
+        function updateTermsQuery(query) {
             let queryString = qs.stringify(query);
 
             getTerms(URLTaxonomyID, queryString).then(data => {
@@ -145,79 +115,49 @@ registerBlockType('tainacan/terms-list', {
                     terms.push(prepareTerm(term));
                 });
 
-                setAttributes({terms: terms});
-                setAttributes({terms2: data});
-                setContent(terms);
-            });
-        }
-        
-        function parseURL(tainacanURLP) {
-            tainacanURL = tainacanURLP;
-            setAttributes({tainacanURL: tainacanURLP});
-
-            if (!tainacanURLP || !tainacanURLP.includes('tainacan_admin')){
-                setAttributes({query: ''});
-                setAttributes({URLTaxonomyID: ''});
-                setAttributes({termsPerPage: 0});
-                setAttributes({terms: []});
-                setAttributes({terms2: []});
-
-                setContent([]);
-
-                return true;
-            }
-
-            let tainacanURLSplited = tainacanURL.split('?');
-
-            let rawQuery = tainacanURLSplited[2];
-            let rawURL = tainacanURLSplited[1];
-
-            let parsedQuery = qs.parse(rawQuery);
-
-            if(parsedQuery.fetch_only && !parsedQuery.fetch_only.includes('title')){
-                parsedQuery.fetch_only += ',title';
-            }
-
-            let URLTaxID = rawURL.match(/\/(\d+)\/?/);
-            URLTaxonomyID = URLTaxID != undefined ? URLTaxID[1]: URLTaxID;
-            console.log(URLTaxonomyID)
-            getTerms(URLTaxonomyID, qs.stringify(parsedQuery)).then(data => {
-                terms = [];
-                setAttributes({terms: terms});
-
-                data.map((term) => {
-                    terms.push(prepareTerm(term));
+                setAttributes({ 
+                    terms: terms, 
+                    URLTaxonomyID: URLTaxonomyID
                 });
-
-                setAttributes({query: parsedQuery});
-                setAttributes({URLTaxonomyID: URLTaxonomyID});
-                setAttributes({termsPerPage: Number(parsedQuery.perpage)});
-                setAttributes({terms: terms});
-                setAttributes({terms2: data});
                 setContent(terms);
             });
         }
 
-        function mountBlock(termsA) {
-            let termsP = [];
+        function mountBlock(termsFromHTML) {
+            let termsOnComponent = [];
 
-            for (const term of termsA){
-                termsP.push(prepareTerm(term));
+            for (const term of termsFromHTML){
+                termsOnComponent.push(prepareTerm(term));
             }
 
-            terms = termsP;
-            setAttributes({terms: termsP});
-            setContent(termsP);
+            terms = termsOnComponent;
+            setAttributes({ terms: termsOnComponent });
+            setContent(termsOnComponent);
         }
 
-        if(content && content.length && content[0].type){
-            mountBlock(terms);
+        function fetchTaxonomies() {
+            isLoadingTaxonomies = true;
+            taxonomies = [];
+
+            tainacan.get(`/taxonomies/?nopaging=1`)
+                .then(response => {
+                    setAttributes({ 
+                        isLoadingTaxonomies: false, 
+                        taxonomies: response.data
+                    });
+
+                    return response.data;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
         }
 
-        // function fetchTaxonomies(query) {
-        //     console.log(query)
-        //     return ['1', '2', '3'];
-        // }
+        function setTaxonomyID( { selectedTaxonomyID } ) {
+            URLTaxonomyID = selectedTaxonomyID;
+            query.number = !termsPerPage ? 1 : termsPerPage;
+            updateTermsQuery(query);
+        }
 
         // const completers = [{
         //     name: 'taxonomy-list-autocomplete',
@@ -228,36 +168,46 @@ registerBlockType('tainacan/terms-list', {
         //     )
         // }]
 
+        if (taxonomies == null) 
+            fetchTaxonomies();
+
+        if(content && content.length && content[0].type)
+            mountBlock(terms);
+        
         return (
             <div className={className}>
 
                 <div>
                     <InspectorControls>
                         <div style={{marginTop: '20px'}}>
-                            <CheckboxControl
-                                heading={__('Show terms count', 'tainacan')}
-                                label={__('yes', 'tainacan')}
-                                checked={ showItemsCount }
-                                onChange={ ( isChecked ) => {
-                                    showItemsCount = isChecked;
+                            <QueryControls
+                                    numberOfItems={ termsPerPage }
+                                    onNumberOfItemsChange={
+                                        (numberOfItems) => {
+                                            query.number = !numberOfItems ? 1 : numberOfItems;
+                                            termsPerPage = query.number;
 
-                                    mountBlock(terms2);
+                                            setAttributes({ termsPerPage: termsPerPage });
 
-                                    setAttributes({showItemsCount: isChecked});
-                                } }
-                            />
+                                            _.debounce(updateTermsQuery(query), 300);
+                                        }
+                                    }
+                                />
                         </div>
                     </InspectorControls>
                 </div>
 
-                { isSelected ? (
-                        <div style={{
-                            marginBottom: '20px',
-                        }}>
-                            <Button
-                                isDefault
-                                onClick={() => setAttributes({isOpen: true})}>{ terms.length ? __('Update terms list', 'tainacan') : __('Add term', 'tainacan')}</Button>
-                        </div>
+                { isSelected ? 
+                    ( isLoadingTaxonomies == true ? 
+                        <Spinner />
+                        :
+                        (
+                            <SelectControl 
+                                label={__('Select a taxonomy', 'tainacan')}
+                                value={ URLTaxonomyID }
+                                options={ taxonomies.map((taxonomy) => ({ label: taxonomy.name, value: taxonomy.id })) }
+                                onChange={ ( selectedTaxonomyID ) => { setTaxonomyID( { selectedTaxonomyID } ) } }/>
+                        )
                     ) : null
                 }
 
@@ -265,13 +215,13 @@ registerBlockType('tainacan/terms-list', {
                     <Placeholder
                         icon={(
                             <img
-                                width={96}
+                                width={148}
                                 src={`${tainacan_plugin.base_url}/admin/images/tainacan_logo_header.svg`}
                                 alt="Tainacan Logo"/>
                         )}
                     />) : null
                 }
-
+                
                 {/* <Autocomplete completers={ completers }>
                     { ( { isExpanded, listBoxId, activeId } ) => (
                         <div
@@ -286,72 +236,13 @@ registerBlockType('tainacan/terms-list', {
                     ) }
                 </Autocomplete> */}
 
-                { isOpen ?
-                    <Modal
-                        shouldCloseOnClickOutside={ false }
-                        shouldCloneOnEsc={false}
-                        focusOnMount={false}
-                        title={ __('Add terms', 'tainacan') }
-                        onRequestClose={ () => setAttributes({isOpen: false}) }>
-
-                        <div>
-                            <TextareaControl
-                                label={__(`Paste a Tainacan sharing URL for a Taxonomy terms list`, 'tainacan')}
-                                type="url"
-                                value={tainacanURL}
-                                rows={8}
-                                onChange={ (tainacanURL) => parseURL( tainacanURL ) }
-                            />
-                        </div>
-
-                        { Object.keys(query).length && query.perpage && tainacanURL ? (
-                            <div>
-                                <QueryControls
-                                    numberOfItems={termsPerPage}
-                                    onNumberOfItemsChange={
-                                        (numberOfItems) => {
-                                            query.perpage = !numberOfItems ? 1 : numberOfItems;
-                                            termsPerPage = query.perpage;
-
-                                            setAttributes({termsPerPage: termsPerPage});
-
-                                            _.debounce(updateQuery(query), 300);
-                                        }
-                                    }
-                                />
-                            </div>
-                            ) : null
-                        }
-
-                        <div>
-                            <Button isDefault onClick={ () => setAttributes({isOpen: false}) }>
-                                { __('Close', 'tainacan') }
-                            </Button>
-                        </div>
-                    </Modal> : null }
-
-                <div style={{ margin: '12px' }}>
-                    <ul
-                            style={{
-                                MozColumnCount: 4,
-                                MozColumnGap: '7rem',
-                                MozColumnRule: 'none',
-                                WebkitColumnCount: 4,
-                                WebkitColumnGap: '7rem',
-                                WebkitColumnRule: 'none',
-                                columnCount: 4,
-                                columnGap: '7rem',
-                                columnRule: 'none'
-                            }}>
-                        { terms }
-                    </ul>
-                </div>
+                <ul class="terms-list">{ terms }</ul>
             </div>
         );
     },
     save({ attributes }){
         const { content } = attributes;
-
+        console.log("Salvando...")
         return <div>{ content }</div>
     }
 });
