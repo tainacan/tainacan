fe3866458cd016bd1073eaab4c5ee9c044235875
Eduardo Humberto <eduardo.humberto1992@gmail.com>
2019-03-12 21:25:04 -0300

    test role author to insert items in his own collection (#205)

--- tests/test-api-items.php
+++ tests/test-api-items.php
@@ -408,11 +408,12 @@ class TAINACAN_REST_Items_Controller extends TAINACAN_UnitApiTestCase {
     /**
      * @group api_item_author
      */
-	public function test_create_item_as_auhor(){
+	public function test_create_item_as_auhor()
+    {
 
-	    //create user as tainacan author
+        //create user as tainacan author
 
-        $new_user = $this->factory()->user->create(array( 'role' => 'tainacan-author' ));
+        $new_user = $this->factory()->user->create(array('role' => 'author'));
         //$new_user = $this->factory()->user->create(array( 'role' => 'administrator' ));
         wp_set_current_user($new_user);
         $user_id = get_current_user_id();
@@ -421,16 +422,16 @@ class TAINACAN_REST_Items_Controller extends TAINACAN_UnitApiTestCase {
         // create collection as auto-draft
 
         $collection_JSON = json_encode([
-            'name'         => 'TesteJsonAdd',
-            'description'  => 'Teste JSON',
+            'name' => 'TesteJsonAdd',
+            'description' => 'Teste JSON',
             "status" => "auto-draft"
         ]);
 
         $request = new \WP_REST_Request('POST', $this->namespace . '/collections');
         $request->set_body($collection_JSON);
 
-        $response = $this->server->dispatch( $request );
-        $this->assertEquals( 201, $response->get_status(), sprintf('response: %s', print_r($response, true)) );
+        $response = $this->server->dispatch($request);
+        $this->assertEquals(201, $response->get_status(), sprintf('response: %s', print_r($response, true)));
 
         $collection = $response->get_data();
         $id = $collection['id'];
@@ -455,18 +456,18 @@ class TAINACAN_REST_Items_Controller extends TAINACAN_UnitApiTestCase {
         // try create item in own collection
 
         $item_json = json_encode([
-            'title'       => 'SCRUM',
+            'title' => 'SCRUM',
             'description' => 'Um framework ágil para gerenciamento de produto.',
             "status" => "auto-draft"
         ]);
 
-        $request  = new \WP_REST_Request('POST', $this->namespace . '/collection/' . $id . '/items');
+        $request = new \WP_REST_Request('POST', $this->namespace . '/collection/' . $id . '/items');
         $request->set_body($item_json);
 
         $response = $this->server->dispatch($request);
 
-        $this->assertEquals(403, $response->get_status());
-       // $this->assertEquals(201, $response->get_status());
+        //$this->assertEquals(403, $response->get_status());
+        $this->assertEquals(201, $response->get_status());
     }
 }
 
