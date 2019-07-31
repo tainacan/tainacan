e480adb8ad5119796ae794c24862550d205911c4
Mateus Machado Luna <mateus.m.luna@gmail.com>
2019-07-31 15:18:31 -0300

    Better emty state label for items list when using custom metadata sorting.

--- src/admin/pages/lists/items-page.vue
+++ src/admin/pages/lists/items-page.vue
@@ -779,7 +779,7 @@
                                 <i class="tainacan-icon tainacan-icon-36px tainacan-icon-items" />
                             </span>
                         </p>
-                        <p v-if="status == undefined || status == ''">{{ hasFiltered ? $i18n.get('info_no_item_found_filter') : $i18n.get('info_no_item_created') }}</p>
+                        <p v-if="status == undefined || status == ''">{{ hasFiltered ? (isSortingByCustomMetadata ? $i18n.get('info_no_item_found') : $i18n.get('info_no_item_found_filter')) : $i18n.get('info_no_item_created') }}</p>
                         <p
                                 v-for="(statusOption, index) of $statusHelper.getStatuses()"
                                 :key="index"
@@ -788,7 +788,7 @@
                         </p>
 
                         <router-link
-                                v-if="!hasFiltered && (status == undefined || status == '') && !$route.query.iframemode"
+                                v-if="!isSortingByCustomMetadata && !hasFiltered && (status == undefined || status == '') && !$route.query.iframemode"
                                 id="button-create-item"
                                 tag="button"
                                 class="button is-secondary"
