f60884309b2a98294702ec1a6f7ba82e9802b392
Mateus Machado Luna <mateus.m.luna@gmail.com>
2019-09-30 15:16:38 -0300

    Fixes style for datepicker on admin list.

--- src/admin/pages/lists/activities-page.vue
+++ src/admin/pages/lists/activities-page.vue
@@ -32,7 +32,7 @@
                         v-if="tab != 'processes'"
                         class="sub-header">
 
-                    <b-field>
+                    <b-field class="header-item">
                         <b-datepicker
                                 ref="datepicker"
                                 :placeholder="$i18n.get('instruction_filter_activities_date')"
@@ -78,21 +78,23 @@
                         </p>
                     </b-field>
 
-                    <div class="control has-icons-right  is-small is-clearfix">
-                        <input
-                                class="input is-small"
-                                :placeholder="$i18n.get('instruction_search')"
-                                type="search"
-                                :aria-label="$i18n.get('instruction_search') + ' ' + $i18n.get('activities')"
-                                autocomplete="on"
-                                v-model="searchQuery"
-                                @keyup.enter="searchActivities()">
-                        <span
-                                @click="searchActivities()"
-                                class="icon is-right">
-                            <i class="tainacan-icon tainacan-icon-search" />
-                        </span>
-                    </div>
+                    <b-field class="header-item">
+                        <div class="control has-icons-right  is-small is-clearfix">
+                            <input
+                                    class="input is-small"
+                                    :placeholder="$i18n.get('instruction_search')"
+                                    type="search"
+                                    :aria-label="$i18n.get('instruction_search') + ' ' + $i18n.get('activities')"
+                                    autocomplete="on"
+                                    v-model="searchQuery"
+                                    @keyup.enter="searchActivities()">
+                            <span
+                                    @click="searchActivities()"
+                                    class="icon is-right">
+                                <i class="tainacan-icon tainacan-icon-search" />
+                            </span>
+                        </div>
+                    </b-field>
                 </div>
 
                 <activities-list
@@ -463,6 +465,7 @@
         width: 100%;
 
         .header-item {
+            margin-bottom: 0 !important;
 
             &:not(:last-child) {
                 padding-right: 0.5em;
@@ -479,6 +482,7 @@
             .button {
                 display: flex;
                 align-items: center;
+                border-radius: 0 !important;
             }
             
             .field {
@@ -493,6 +497,15 @@
             .gray-icon i::before {
                 font-size: 1.3125rem !important;
                 max-width: 26px;
+            }
+
+            .icon {
+                pointer-events: all;
+                cursor: pointer;
+                color: $blue5;
+                height: 27px;
+                font-size: 18px !important;
+                height: 1.85rem !important;
             }
         }
 
