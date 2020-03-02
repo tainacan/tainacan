4a9734e3b4625365761c254eb66239e645e138a0
Mateus Machado Luna <mateus.m.luna@gmail.com>
2020-03-02 09:43:16 -0300

    Verification for nonce presence on tainacan_plugin variable.

--- src/views/admin/js/axios.js
+++ src/views/admin/js/axios.js
@@ -4,13 +4,15 @@ export const tainacan = axios.create({
     baseURL: tainacan_plugin.tainacan_api_url
 });
 
-tainacan.defaults.headers.common['X-WP-Nonce'] = tainacan_plugin.nonce;
+if (tainacan_plugin.nonce)
+    tainacan.defaults.headers.common['X-WP-Nonce'] = tainacan_plugin.nonce;
 
 export const wp = axios.create({
     baseURL: tainacan_plugin.wp_api_url
 });
 
-wp.defaults.headers.common['X-WP-Nonce'] = tainacan_plugin.nonce;
+if (tainacan_plugin.nonce)
+    wp.defaults.headers.common['X-WP-Nonce'] = tainacan_plugin.nonce;
 
 export const CancelToken = axios.CancelToken;
 export const isCancel = axios.isCancel;
