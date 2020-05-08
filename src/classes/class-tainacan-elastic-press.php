82f365d82064091c6612897ef23be60ce5c0f262
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-05-08 15:35:44 -0300

    Parses the term_id as an integer in Elastic_Press::format_aggregations_facet method

--- src/classes/class-tainacan-elastic-press.php
+++ src/classes/class-tainacan-elastic-press.php
@@ -727,13 +727,13 @@ class Elastic_Press {
 				$buckets = ($has_include == false ? $aggregation['buckets'] : $aggregation[$key]['buckets']);
 				foreach ($buckets as $term) {
 					if ($has_include) {
-						$term_id = $term['key'];
+						$term_id = intval($term['key']);
 						$doc_count = $term['doc_count'];
 					} else {
-						$term_id = $term['key'][$key];
+						$term_id = intval($term['key'][$key]);
 						$doc_count = $term['doc_count'];
 					}
-					if ($term_id == '') continue;
+					if ($term_id === 0) continue;
 
 					$term_object = \Tainacan\Repositories\Terms::get_instance()->fetch($term_id, $taxonomy_slug);
 					$count_query = $wpdb->prepare("SELECT COUNT(term_id) FROM $wpdb->term_taxonomy WHERE parent = %d", $term_id);
