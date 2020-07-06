f4b4295e073010da57c23d922c5b52d769b984ab
mateuswetah <mateus.m.luna@gmail.com>
2020-07-06 15:33:30 -0300

    More importers form adjustments.

--- src/classes/exporter/class-tainacan-csv.php
+++ src/classes/exporter/class-tainacan-csv.php
@@ -282,77 +282,73 @@ class CSV extends Exporter {
 	public function options_form() {
 		ob_start();
 	   ?>
-	   <div class="columns">
-			<div class="column">
-				<div class="field">
-					<label class="label"><?php _e('CSV Delimiter', 'tainacan'); ?></label>
-					<span class="help-wrapper">
-							<a class="help-button has-text-secondary">
-								<span class="icon is-small">
-									<i class="tainacan-icon tainacan-icon-help" ></i>
-								</span>
-							</a>
-							<div class="help-tooltip">
-								<div class="help-tooltip-header">
-									<h5><?php _e('CSV Delimiter', 'tainacan'); ?></h5>
-								</div>
-								<div class="help-tooltip-body">
-									<p><?php _e('The character used to separate each column in your CSV (e.g. , or ;)', 'tainacan'); ?></p>
-								</div>
-							</div> 
-					</span>
-					<div class="control is-clearfix">
-						<input class="input" type="text" name="delimiter" maxlength="1" value="<?php echo $this->get_option('delimiter'); ?>">
-					</div>
-				</div>
-			</div>
-			<div class="column">
-				<div class="field">
-					<label class="label"><?php _e('Multivalued metadata delimiter', 'tainacan'); ?></label>
-					<span class="help-wrapper">
-							<a class="help-button has-text-secondary">
-								<span class="icon is-small">
-									<i class="tainacan-icon tainacan-icon-help" ></i>
-								</span>
-							</a>
-							<div class="help-tooltip">
-								<div class="help-tooltip-header">
-									<h5><?php _e('Multivalued metadata delimiter', 'tainacan'); ?></h5>
-								</div>
-								<div class="help-tooltip-body">
-									<p><?php _e('The character used to separate each value inside a cell with multiple values (e.g. ||). Note that the target metadatum must accept multiple values.', 'tainacan'); ?></p>
-								</div>
-							</div> 
-					</span>
-					<div class="control is-clearfix">
-						<input class="input" type="text" name="multivalued_delimiter" value="<?php echo $this->get_option('multivalued_delimiter'); ?>">
-					</div>
-				</div>
-			</div>
-			<div class="column">
-				<div class="field">
-					<label class="label"><?php _e('Enclosure', 'tainacan'); ?></label>
-					<span class="help-wrapper">
-							<a class="help-button has-text-secondary">
-								<span class="icon is-small">
-									<i class="tainacan-icon tainacan-icon-help" ></i>
-								</span>
-							</a>
-							<div class="help-tooltip">
-								<div class="help-tooltip-header">
-									<h5><?php _e('Enclosure', 'tainacan'); ?></h5>
-								</div>
-								<div class="help-tooltip-body">
-									<p><?php _e('The character that wraps the content of each cell in your CSV. (e.g. ")', 'tainacan'); ?></p>
-								</div>
-							</div> 
-					</span>
-					<div class="control is-clearfix">
-						<input class="input" type="text" name="enclosure" value="<?php echo esc_attr($this->get_option('enclosure')); ?>">
-					</div>
-				</div>
+		<div class="field">
+			<label class="label"><?php _e('CSV Delimiter', 'tainacan'); ?></label>
+			<span class="help-wrapper">
+					<a class="help-button has-text-secondary">
+						<span class="icon is-small">
+							 <i class="tainacan-icon tainacan-icon-help" ></i>
+						 </span>
+					</a>
+					<div class="help-tooltip">
+						<div class="help-tooltip-header">
+							<h5><?php _e('CSV Delimiter', 'tainacan'); ?></h5>
+						</div>
+						<div class="help-tooltip-body">
+							<p><?php _e('The character used to separate each column in your CSV (e.g. , or ;)', 'tainacan'); ?></p>
+						</div>
+					</div> 
+			</span>
+			<div class="control is-clearfix">
+				<input class="input" type="text" name="delimiter" maxlength="1" value="<?php echo $this->get_option('delimiter'); ?>">
 			</div>
 		</div>
+		
+		<div class="field">
+			<label class="label"><?php _e('Multivalued metadata delimiter', 'tainacan'); ?></label>
+			<span class="help-wrapper">
+					<a class="help-button has-text-secondary">
+						<span class="icon is-small">
+							 <i class="tainacan-icon tainacan-icon-help" ></i>
+						 </span>
+					</a>
+					<div class="help-tooltip">
+						<div class="help-tooltip-header">
+							<h5><?php _e('Multivalued metadata delimiter', 'tainacan'); ?></h5>
+						</div>
+						<div class="help-tooltip-body">
+							<p><?php _e('The character used to separate each value inside a cell with multiple values (e.g. ||). Note that the target metadatum must accept multiple values.', 'tainacan'); ?></p>
+						</div>
+					</div> 
+			</span>
+			<div class="control is-clearfix">
+				<input class="input" type="text" name="multivalued_delimiter" value="<?php echo $this->get_option('multivalued_delimiter'); ?>">
+			</div>
+		</div>
+		
+		<div class="field">
+			<label class="label"><?php _e('Enclosure', 'tainacan'); ?></label>
+			<span class="help-wrapper">
+					<a class="help-button has-text-secondary">
+						<span class="icon is-small">
+							 <i class="tainacan-icon tainacan-icon-help" ></i>
+						 </span>
+					</a>
+					<div class="help-tooltip">
+						<div class="help-tooltip-header">
+							<h5><?php _e('Enclosure', 'tainacan'); ?></h5>
+						</div>
+						<div class="help-tooltip-body">
+							<p><?php _e('The character that wraps the content of each cell in your CSV. (e.g. ")', 'tainacan'); ?></p>
+						</div>
+					</div> 
+			</span>
+			<div class="control is-clearfix">
+				<input class="input" type="text" name="enclosure" value="<?php echo esc_attr($this->get_option('enclosure')); ?>">
+			</div>
+		</div>
+		
+		
 	   
 	   <?php 
 	   
