26f1b22b63c51e630a9da1601af983d3b0f3a9d3
vnmedeiros <vnicius.nm.ba@gmail.com>
2020-08-06 10:27:20 -0300

    add count draft items in total_items for exporter csv

--- src/classes/api/endpoints/class-tainacan-rest-exporters-controller.php
+++ src/classes/api/endpoints/class-tainacan-rest-exporters-controller.php
@@ -159,7 +159,7 @@ class REST_Exporters_Controller extends REST_Controller {
 						if (is_array($value) && isset($value['id'])) {
 							$collection = $this->collections_repository->fetch($value['id']);
 							$total_items = wp_count_posts( $collection->get_db_identifier(), 'readable' );
-							$value['total_items'] = ($total_items->publish + $total_items->private);
+							$value['total_items'] = ($total_items->publish + $total_items->private + $total_items->draft);
 							$exporter->add_collection($value);
 							continue;
 						} else {
