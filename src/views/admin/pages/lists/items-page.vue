7788bf9cff9f9d08c40fb81bd9119a6c96b67d99
mateuswetah <mateus.m.luna@gmail.com>
2020-11-04 17:23:10 -0300

    Fixes user prefs not being saved properly for repository level settings.

--- src/views/admin/pages/lists/items-page.vue
+++ src/views/admin/pages/lists/items-page.vue
@@ -990,8 +990,8 @@
                                     let prefsFetchOnlyObject = this.$userPrefs.get(prefsFetchOnly) ? typeof this.$userPrefs.get(prefsFetchOnly) != 'string' ? this.$userPrefs.get(prefsFetchOnly) : this.$userPrefs.get(prefsFetchOnly).replace(/,null/g, '').split(',') : [];
                                     let prefsFetchOnlyMetaObject = this.$userPrefs.get(prefsFetchOnlyMeta) ? this.$userPrefs.get(prefsFetchOnlyMeta).split(',') : [];
 
-                                    let thumbnailMetadatumDisplay = (!this.isRepositoryLevel && this.collection.hide_items_thumbnail_on_lists == 'yes') ? null : (prefsFetchOnlyObject ? (prefsFetchOnlyObject[0] != null) : true);
-                                    
+                                    let thumbnailMetadatumDisplay = (!this.isRepositoryLevel && this.collection.hide_items_thumbnail_on_lists == 'yes') ? null : (prefsFetchOnlyObject ? (prefsFetchOnlyObject[0] != 'null') : true);
+
                                     if (this.isRepositoryLevel || this.collection.hide_items_thumbnail_on_lists != 'yes') {
                                         metadata.push({
                                             name: this.$i18n.get('label_thumbnail'),
@@ -1073,8 +1073,8 @@
                                         }
                                     }
 
-                                    let creationDateMetadatumDisplay = prefsFetchOnlyObject ? (prefsFetchOnlyObject[1] != null) : true;
-                                    let authorNameMetadatumDisplay = prefsFetchOnlyObject ? (prefsFetchOnlyObject[2] != null) : true;
+                                    let creationDateMetadatumDisplay = prefsFetchOnlyObject ? (prefsFetchOnlyObject[1] != 'null') : true;
+                                    let authorNameMetadatumDisplay = prefsFetchOnlyObject ? (prefsFetchOnlyObject[2] != 'null') : true;
 
                                     // Creation date and author name should appear only on admin.
                                     metadata.push({
