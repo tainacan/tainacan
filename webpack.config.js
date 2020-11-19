f0e6d4c1ab6d286ad6054a31379b663a2d3481b4
mateuswetah <mateus.m.luna@gmail.com>
2020-11-19 10:08:39 -0300

    Updates terser plugin version to 3.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -113,34 +113,20 @@ module.exports = {
 const production = false;
 
 if (production === true) {
+    process.env.NODE_ENV = 'production';
     const TerserPlugin = require('terser-webpack-plugin');
 
     console.log(`Production: ${production}`);
 
     module.exports.mode = 'production';
 
-    module.exports.devtool = '';
+    module.exports.devtool = undefined;
 
     module.exports.plugins = (module.exports.plugins || []).concat([
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify('production')
             }
-        }),
-        new TerserPlugin({
-            parallel: true,
-            sourceMap: false,
-            cache: true,
-            terserOptions: {
-                // We preserve function names that start with capital letters as
-                // they're _likely_ component names, and these are useful to have
-                // in tracebacks and error messages.
-                keep_fnames: /__|_x|_n|_nx|sprintf|^[A-Z].+$/,
-                output: {
-                    comments: /translators:/i,
-                },
-            },
-            extractComments: false,
         }),
         new webpack.LoaderOptionsPlugin({
             minimize: true
@@ -154,6 +140,32 @@ if (production === true) {
             'Swiper$': 'swiper/js/swiper.min.js'
         }
     }
+
+    module.exports.optimization = {
+        sideEffects: true,
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                parallel: true,
+                sourceMap: false,
+                cache: true,
+                terserOptions: {
+                    // We preserve function names that start with capital letters as
+                    // they're _likely_ component names, and these are useful to have
+                    // in tracebacks and error messages.
+                    keep_fnames: /__|_x|_n|_nx/,
+                    mangle: {
+                        keep_fnames: /__|_x|_n|_nx/,
+                    },
+                    output: {
+                        comments: /translators:/i,
+                    },
+                },
+                extractComments: false,
+            })
+        ],
+    };
+
 } else {
     console.log(`Production: ${production}`);
 
