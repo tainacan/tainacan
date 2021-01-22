912ae47e5ca6daf8074fea11c2f17617e98d251e
mateuswetah <mateus.m.luna@gmail.com>
2021-01-22 09:46:09 -0300

    Fixes undefined warning for enabled view modes on repository level lists.

--- src/views/theme-search/js/view-modes-mixin.js
+++ src/views/theme-search/js/view-modes-mixin.js
@@ -32,7 +32,7 @@ export const viewModesMixin = {
         }
     },
     mounted() {
-        this.isSlideshowViewModeEnabled = this.enabledViewModes.findIndex((viewMode) => viewMode == 'slideshow') >= 0;
+        this.isSlideshowViewModeEnabled = (this.enabledViewModes && Array.isArray(this.enabledViewModes)) ? (this.enabledViewModes.findIndex((viewMode) => viewMode == 'slideshow') >= 0) : false;
     },
     methods: {
         getItemLink(itemUrl, index) {
