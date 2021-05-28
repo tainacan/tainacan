50760fd89ed5dfed06ad88894109828a60eb4e59
mateuswetah <mateus.m.luna@gmail.com>
2021-05-28 16:18:10 -0300

    Checks numeric values properly on has_value function of item metadata entity

--- src/classes/entities/class-tainacan-item-metadata-entity.php
+++ src/classes/entities/class-tainacan-item-metadata-entity.php
@@ -414,7 +414,7 @@ class Item_Metadata_Entity extends Entity {
 			return $this->has_value;
 		
 		$value = $this->get_value();
-		$this->has_value = (is_array($value)) ? !empty(array_filter($value)) : !empty($value);
+		$this->has_value = (is_array($value)) ? !empty(array_filter($value)) : (is_numeric($value) || !empty($value));
 		return $this->has_value;
 	}
 	
