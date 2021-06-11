80d254a0a679f91aa73bb06145a4e94155358875
vnmedeiros <vnicius.nm.ba@gmail.com>
2021-06-11 15:00:19 -0300

    fix: add parameter on action `tainacan-registered-exporters`

--- src/classes/exporter/class-tainacan-export-handler.php
+++ src/classes/exporter/class-tainacan-export-handler.php
@@ -20,16 +20,16 @@ class Export_Handler {
 			'class_name' => '\Tainacan\Exporter\CSV'
 		]);
 
-        $this->register_exporter([
-            'name' => 'Vocabulary CSV',
-            'description' => __('Allows you to export a taxonomy to a CSV file', 'tainacan'),
-            'slug' => 'vocabularycsv',
-            'class_name' => '\Tainacan\Exporter\Term_Exporter',
-            'manual_mapping' => false,
-            'manual_collection' => false
-        ]);
-		
-		do_action('tainacan-register-exporters');
+		$this->register_exporter([
+			'name' => 'Vocabulary CSV',
+			'description' => __('Allows you to export a taxonomy to a CSV file', 'tainacan'),
+			'slug' => 'vocabularycsv',
+			'class_name' => '\Tainacan\Exporter\Term_Exporter',
+			'manual_mapping' => false,
+			'manual_collection' => false
+		]);
+
+		do_action('tainacan-register-exporters', $this);
 
 		add_action( 'tainacan-enqueue-admin-scripts', array($this, 'enqueue_scripts') );
 	}
