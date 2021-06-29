7d17c2174c7d37772492b51461bfaa3e0b57ef3f
vnmedeiros <vnicius.nm.ba@gmail.com>
2021-06-29 15:11:58 -0300

    fix: add unsupported function `is_post_status_viewable` on previous WP version

--- src/classes/tainacan-utils.php
+++ src/classes/tainacan-utils.php
@@ -13,3 +13,28 @@ function tainacan_get_api_postdata() {
 	$post = json_decode($postdata);
 	return $post;
 }
+
+/**
+ * Retrieve if the post status is viewable
+ * @return boollean
+ */
+if(!function_exists("is_post_status_viewable")) {
+	function is_post_status_viewable( $post_status ) {
+		if ( is_scalar( $post_status ) ) {
+			$post_status = \get_post_status_object( $post_status );
+			if ( ! $post_status ) {
+				return false;
+			}
+		}
+	 
+		if (
+			! \is_object( $post_status ) ||
+			$post_status->internal ||
+			$post_status->protected
+		) {
+			return false;
+		}
+	 
+		return $post_status->publicly_queryable || ( $post_status->_builtin && $post_status->public );
+	}
+}
