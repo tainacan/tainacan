bbbec638752e5f25bf4d272acc3e4d7b15ea9c74
mateuswetah <mateus.m.luna@gmail.com>
2021-06-30 17:58:10 -0300

    Adds autocomplete option to text input. #575

--- src/views/tainacan-admin-i18n.php
+++ src/views/tainacan-admin-i18n.php
@@ -870,6 +870,7 @@ return apply_filters( 'tainacan-admin-i18n', [
 	'info_no_taxonomy_metadata_created'		 		 =>	__( 'No taxonomy metadata created yet', 'tainacan'),
 	'label_amount_of_metadata_of_type'	 			 =>	__( 'Amount of metadata of this type', 'tainacan'),
 	'info_child_terms_chart'						 => __( 'Click on the term bar on the chart aside to see its child terms (if any) in this panel', 'tainacan' ),
+	'info_metadata_autocomplete_suggestions'		 => __( 'Some values already used on this metadatum:', 'tainacan' ),
 	
 	/* Activity actions */
 	'action_update-metadata-value'	=> __( 'Item Metadata Value Updates', 'tainacan'),
