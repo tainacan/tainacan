eb7b156c10afc623c635ec19e9249506600336f1
vnmedeiros <vnicius.nm.ba@gmail.com>
2021-07-14 12:57:37 -0300

    fix: test item blurhash

--- tests/test-items.php
+++ tests/test-items.php
@@ -517,11 +517,10 @@ class Items extends TAINACAN_UnitTestCase {
 	}
 
 	function test_item_blurhash() {
-		$item = new Entities\Item();
 		$orig_file = './tests/attachment/tainacan.jpg';
 		$test_file = '/tmp/tainacan.jpg';
 		copy( $orig_file, $test_file );
-		$blurhash = $item->get_image_blurhash($test_file, 40,40);
+		$blurhash = \Tainacan\Media::get_instance()->get_image_blurhash($test_file, 40,40);
 		$this->assertContains($blurhash, ['V4P?:h00Rj~qM{of%MRjWBRjD%%MRjayofj[%M-;RjRj', 'LATI:i~qNG~W~qNGxaNGM|xaNGxa']);
 	}
 
