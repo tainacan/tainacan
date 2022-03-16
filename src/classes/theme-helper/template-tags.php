b70b02a30bcb90646f89543bad019d20e87d6fb1
mateuswetah <mateus.m.luna@gmail.com>
2022-03-16 11:26:45 -0300

    Loads photoswipe from URL if hash is passed. #548.

--- src/classes/theme-helper/template-tags.php
+++ src/classes/theme-helper/template-tags.php
@@ -346,10 +346,6 @@ function tainacan_get_the_media_component(
 	ob_start();
 
 	if ( $args['has_media_main'] || $args['has_media_thumbs'] ) :
-
-		// Modal lightbox layer for rendering photoswipe
-		if (!$args['disable_lightbox'])
-			add_action('wp_footer', 'tainacan_get_the_media_modal_layer');
 	
 		wp_enqueue_style( 'tainacan-media-component', $TAINACAN_BASE_URL . '/assets/css/tainacan-gutenberg-block-item-gallery.css', array(), TAINACAN_VERSION);
 
@@ -581,75 +577,6 @@ function tainacan_get_the_media_component_slide( $args = array() ) {
 	ob_end_clean();
 
 	return $content;
-}
-
-/**
- * Div with content necessay to render the photowipe modal
- *
- * @return string
- */
-function tainacan_get_the_media_modal_layer() {
-?> 
-    <!-- Root element of PhotoSwipe lightbox. Must have class pswp. -->
-    <div class="tainacan-photoswipe-layer pswp" tabindex="-1" role="dialog" aria-hidden="true">
-
-        <!-- Background of PhotoSwipe. 
-                It's a separate element, as animating opacity is faster than rgba(). -->
-        <div class="pswp__bg"></div>
-
-        <!-- Slides wrapper with overflow:hidden. -->
-        <div class="pswp__scroll-wrap">
-
-            <!-- Container that holds slides. PhotoSwipe keeps only 3 slides in DOM to save memory. -->
-            <!-- don't modify these 3 pswp__item elements, data is added later on. -->
-            <div class="pswp__container">
-                <div class="pswp__item"></div>
-                <div class="pswp__item"></div>
-                <div class="pswp__item"></div>
-            </div>
-
-            <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
-            <div class="pswp__ui pswp__ui--hidden">
-
-                <div class="pswp__top-bar">
-
-                    <!--  Controls are self-explanatory. Order can be changed. -->
-                    <div class="pswp__counter"></div>
-
-                    <div class="pswp__name"></div>
-
-                    <button class="pswp__button pswp__button--close" title="<?php __('Close modal (Esc)', 'tainacan') ?>"></button>
-                    <button class="pswp__button pswp__button--share" title="<?php __('Share', 'tainacan') ?>"></button>
-                    <button class="pswp__button pswp__button--fs" title="<?php __('Toggle fullscreen', 'tainacan') ?>"></button>
-                    <button class="pswp__button pswp__button--zoom" title="<?php __('Zoom in/out', 'tainacan') ?>"></button>
-
-                    <!-- Preloader demo https://codepen.io/dimsemenov/pen/yyBWoR -->
-                    <!-- element will get class pswp__preloader--active when preloader is running -->
-                    <div class="pswp__preloader">
-                        <div class="pswp__preloader__icn">
-                            <div class="pswp__preloader__cut">
-                                <div class="pswp__preloader__donut"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-                    <div class="pswp__share-tooltip"></div>
-                </div>
-
-                <button class="pswp__button pswp__button--arrow--left" title="<?php __('Next', 'tainacan') ?>"></button>
-
-                <button class="pswp__button pswp__button--arrow--right" title="<?php __('Previous', 'tainacan') ?>"></button>
-
-                <div class="pswp__caption">
-                    <div class="pswp__caption__center"></div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-<?php
 }
 
 /**
