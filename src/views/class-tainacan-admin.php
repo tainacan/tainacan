8ae1ccf31e5cfe8837c5fcd4436107eb74754874
vnmedeiros <vnicius.nm.ba@gmail.com>
2022-11-24 08:36:17 -0300

    move function `parse_core_metadata_for_advanced_search` to items repository #740

--- src/views/class-tainacan-admin.php
+++ src/views/class-tainacan-admin.php
@@ -327,7 +327,7 @@ class Admin {
 			'api_max_items_per_page'    => $TAINACAN_API_MAX_ITEMS_PER_PAGE,
 			'wp_elasticpress'    		=> \Tainacan\Elastic_Press::get_instance()->is_active(),
 			'item_submission_captcha_site_key' => get_option("tnc_option_recaptch_site_key"),
-			'tainacan_enable_core_metadata_on_advanced_search' => ( defined('TAINACAN_ENABLE_CORE_METADATA_ON_ADVANCED_SEARCH') && true === TAINACAN_ENABLE_CORE_METADATA_ON_ADVANCED_SEARCH),
+			'tainacan_enable_core_metadata_on_advanced_search' => ( defined('TAINACAN_ENABLE_CORE_METADATA_ON_ADVANCED_SEARCH') && true === TAINACAN_ENABLE_CORE_METADATA_ON_ADVANCED_SEARCH ),
 			'tainacan_enable_relationship_metaquery' => ( defined('TAINACAN_ENABLE_RELATIONSHIP_METAQUERY') && true === TAINACAN_ENABLE_RELATIONSHIP_METAQUERY )
 		];
 		
