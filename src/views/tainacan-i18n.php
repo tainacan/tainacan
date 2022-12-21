408fb69d21825d4f9c0ee0ccd6cf99f5bf9dac40
mateuswetah <mateus.m.luna@gmail.com>
2022-12-21 10:40:58 -0300

    Adds warning related to required fields on item submission for draft status. #754.

--- src/views/tainacan-i18n.php
+++ src/views/tainacan-i18n.php
@@ -991,6 +991,7 @@ return apply_filters( 'tainacan-i18n', [
 	'info_try_enabling_search_by_word'				 => __( 'Try enabling the search by words.', 'tainacan' ),
 	'info_try_disabling_search_by_word'				 => __( 'Try disabling the search by words, to search for the complete phrase.', 'tainacan' ),
 	'info_details_about_search_by_word'				 => __( 'They may be located on different metadata and order, but you will still be able to use quotes to group them.', 'tainacan' ),
+	'info_item_submission_draft_status'				 => __( 'Warning: draft items may be submitted even without filling all required metadata.', 'tainacan' ),
 
 	/* Activity actions */
 	'action_update-metadata-value'	=> __( 'Item Metadata Value Updates', 'tainacan'),
