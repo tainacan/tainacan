022058381dc6bae3b37f0e28cb65bbd88f7932d8
mateuswetah <mateus.m.luna@gmail.com>
2023-01-23 10:27:59 -0300

    Adds map icon to map view mode. #693.

--- src/views/admin/pages/lists/items-page.vue
+++ src/views/admin/pages/lists/items-page.vue
@@ -357,18 +357,23 @@
                                 :aria-label="$i18n.get('label_view_mode')"
                                 class="button is-white"
                                 slot="trigger">
-                            <span>
-                                <span class="view-mode-icon icon is-small gray-icon">
-                                    <i 
-                                            :class="{'tainacan-icon-viewtable' : ( adminViewMode == 'table' || adminViewMode == undefined),
-                                                    'tainacan-icon-viewcards' : adminViewMode == 'cards',
-                                                    'tainacan-icon-viewminiature' : adminViewMode == 'grid',
-                                                    'tainacan-icon-viewrecords' : adminViewMode == 'records',
-                                                    'tainacan-icon-viewlist' : adminViewMode == 'list',
-                                                    'tainacan-icon-viewmasonry' : adminViewMode == 'masonry',
-                                                    'tainacan-icon-public' : adminViewMode == 'map' }"
-                                            class="tainacan-icon tainacan-icon-1-25em"/>
-                                </span>
+                            <span class="view-mode-icon icon is-small gray-icon">
+                                <i 
+                                        v-if="adminViewMode !== 'map'"
+                                        :class="{'tainacan-icon-viewtable' : ( adminViewMode == 'table' || adminViewMode == undefined),
+                                                'tainacan-icon-viewcards' : adminViewMode == 'cards',
+                                                'tainacan-icon-viewminiature' : adminViewMode == 'grid',
+                                                'tainacan-icon-viewrecords' : adminViewMode == 'records',
+                                                'tainacan-icon-viewlist' : adminViewMode == 'list',
+                                                'tainacan-icon-viewmasonry' : adminViewMode == 'masonry' }"
+                                        class="tainacan-icon tainacan-icon-1-25em"/>
+                                <svg
+                                        v-else
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        style="fill: var(--tainacan-info-color)">
+                                    <path d="M15,19L9,16.89V5L15,7.11M20.5,3C20.44,3 20.39,3 20.34,3L15,5.1L9,3L3.36,4.9C3.15,4.97 3,5.15 3,5.38V20.5A0.5,0.5 0 0,0 3.5,21C3.55,21 3.61,21 3.66,20.97L9,18.9L15,21L20.64,19.1C20.85,19 21,18.85 21,18.62V3.5A0.5,0.5 0 0,0 20.5,3Z" />
+                                </svg>
                             </span>
                             &nbsp;&nbsp;&nbsp;{{ adminViewMode != undefined ? $i18n.get('label_' + adminViewMode) : $i18n.get('label_table') }}
                             <span class="icon">
@@ -449,8 +454,15 @@
                                 :class="{ 'is-active': adminViewMode == 'map' }"
                                 :value="'map'"
                                 aria-role="listitem">
-                            <span class="icon gray-icon">
-                                <i class="tainacan-icon tainacan-icon-public"/>
+                            <span 
+                                    style="width: 2em; margin-left: -0.45em; padding-right: 6px;"
+                                    class="icon gray-icon">
+                                <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        style="fill: var(--tainacan-info-color)">
+                                    <path d="M15,19L9,16.89V5L15,7.11M20.5,3C20.44,3 20.39,3 20.34,3L15,5.1L9,3L3.36,4.9C3.15,4.97 3,5.15 3,5.38V20.5A0.5,0.5 0 0,0 3.5,21C3.55,21 3.61,21 3.66,20.97L9,18.9L15,21L20.64,19.1C20.85,19 21,18.85 21,18.62V3.5A0.5,0.5 0 0,0 20.5,3Z" />
+                                </svg>
                             </span>
                             <span>{{ $i18n.get('label_map') }}</span>
                         </b-dropdown-item>
