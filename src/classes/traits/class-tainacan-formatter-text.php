b7f6fc9a51858e8bb5fabf31fc9aead2a2332274
mateuswetah <mateus.m.luna@gmail.com>
2025-01-23 17:22:44 -0300

    Allows clickable links in Collection and Term description #936.

--- src/classes/traits/class-tainacan-formatter-text.php
+++ src/classes/traits/class-tainacan-formatter-text.php
@@ -15,7 +15,7 @@ trait Formatter_Text {
 	 * 
 	 * @return string Texts with url's transformed in html tag <a>
 	 */
-	public function make_clickable_links($text) {
+	public static function make_clickable_links($text) {
 		$url = '~((www\.|http:\/\/www\.|http:\/\/|https:\/\/www\.|https:\/\/|ftp:\/\/www\.|ftp:\/\/|ftps:\/\/www\.|ftps:\/\/)[^"<\s]+)(?![^<>]*>|[^"]*?<\/a)~i';
 		$text = preg_replace($url, '<a href="$0" target="_blank" title="$0">$0</a>', $text);
 		$text = str_replace('href="www.', 'href="http://www.', $text);
