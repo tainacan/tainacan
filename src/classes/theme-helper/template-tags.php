807ca4a19fc090ecf8e3189371e59e47a1a7eea5
mateuswetah <mateus.m.luna@gmail.com>
2025-08-27 20:10:59 -0300

    Loads Tainacan Icon fonts in media gallery in case no svg swiper arrow is provided.

--- src/classes/theme-helper/template-tags.php
+++ src/classes/theme-helper/template-tags.php
@@ -394,6 +394,10 @@ function tainacan_get_the_media_component(
 	if ( $args['has_media_main'] || $args['has_media_thumbs'] ) :
 	
 		wp_enqueue_style( 'tainacan-media-component', $TAINACAN_BASE_URL . '/assets/css/tainacan-gutenberg-block-item-gallery.css', array(), TAINACAN_VERSION);
+		
+		if ( !isset($args['swiper_arrows_as_svg']) || !$args['swiper_arrows_as_svg'] )
+			wp_enqueue_style( 'tainacan-fonts', $TAINACAN_BASE_URL . '/assets/css/tainacanicons.css', array(), TAINACAN_VERSION );
+		
 		?>
 
 		<script>
