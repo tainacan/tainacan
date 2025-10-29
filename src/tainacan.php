42b1a3d82b26311485c866922a2020114374eb23
mateuswetah <mateus.m.luna@gmail.com>
2025-10-29 16:38:02 -0300

    Uses same license for readme.txt and plugin file.

--- src/tainacan.php
+++ src/tainacan.php
@@ -11,7 +11,7 @@ Tested up to: 6.8
 Requires PHP: 7.0
 Stable tag: 1.0.0
 Text Domain: tainacan
-License: GPLv2 or later
+License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
