5716b54dce5a9eb0fcb941e321239f94ea312eb2
mateuswetah <mateus.m.luna@gmail.com>
2025-12-26 11:56:33 -0300

    Fixes wrong expirationtime build on OAI-PMH classes.

--- src/classes/oaipmh/class-tainacan-oaipmh-list-identifiers.php
+++ src/classes/oaipmh/class-tainacan-oaipmh-list-identifiers.php
@@ -203,7 +203,7 @@ class OAIPMH_List_Identifiers extends OAIPMH_Expose {
             }
             $this->cursor = (int) $this->deliveredrecords + $this->MAXRECORDS;
             $this->restoken = $this->createResumToken($this->cursor, $this->from,$this->until,$this->sets, $this->metadataPrefix);
-            $this->expirationdatetime = date("Y-m-d\TH:i:s\Z", time() * $this->token_valid);
+            $this->expirationdatetime = date("Y-m-d\TH:i:s\Z", time() + $this->token_valid);
         }
     }
 
--- src/classes/oaipmh/class-tainacan-oaipmh-list-records.php
+++ src/classes/oaipmh/class-tainacan-oaipmh-list-records.php
@@ -287,7 +287,7 @@ class OAIPMH_List_Records extends OAIPMH_Expose {
             }
             $this->cursor = (int) $this->deliveredrecords + $this->MAXRECORDS;
             $this->restoken = $this->createResumToken($this->cursor, $this->from,$this->until,$this->sets, $this->metadataPrefix);
-            $this->expirationdatetime = date("Y-m-d\TH:i:s\Z", time() * $this->token_valid);
+            $this->expirationdatetime = date("Y-m-d\TH:i:s\Z", time() + $this->token_valid);
         }
     }
 
