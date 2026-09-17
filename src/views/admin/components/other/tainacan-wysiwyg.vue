3ee43501605a867124ee3f913ecd4d29cec23a2c
Fred Marvila <marvila@bauhaustech.com>
2026-09-17 19:05:45 -0300

    feat(wysiwyg): add keyboard toolbar guidance

--- src/views/admin/components/other/tainacan-wysiwyg.vue
+++ src/views/admin/components/other/tainacan-wysiwyg.vue
@@ -16,6 +16,16 @@
                 @update:model-value="onUpdate"
                 @focus="onFocus"
                 @blur="onBlur" />
+        <p
+                class="tainacan-wysiwyg-keyboard-hint"
+                aria-hidden="true">
+            {{ $i18n.get('instruction_wysiwyg_toolbar_shortcut') }}
+        </p>
+        <p
+                :id="keyboardHintId"
+                class="sr-only">
+            {{ $i18n.get('instruction_wysiwyg_toolbar_shortcut_screen_reader') }}
+        </p>
     </div>
 </template>
 
@@ -31,6 +41,8 @@ import 'tinymce/plugins/lists';
 import './tainacan-tinymce-skin.scss';
 import contentCss from 'tinymce/skins/content/default/content.css';
 import contentUiCss from 'tinymce/skins/ui/oxide/content.css';
+
+let nextKeyboardHintId = 0;
 
 const EDITOR_INIT = {
     menubar: false,
@@ -142,19 +154,27 @@ export default {
     emits: [ 'update:modelValue', 'focus', 'blur' ],
     data() {
         return {
-            editorInit: {
+            keyboardHintId: `tainacan-wysiwyg-keyboard-hint-${++nextKeyboardHintId}`
+        };
+    },
+    computed: {
+        iframeAriaDescribedby() {
+            return [ this.ariaDescribedby, this.keyboardHintId ].filter(Boolean).join(' ');
+        },
+        editorInit() {
+            return {
                 ...EDITOR_INIT,
                 placeholder: this.placeholder,
                 iframe_attrs: {
                     ...(this.ariaLabelledby
                         ? { 'aria-labelledby': this.ariaLabelledby }
                         : {}),
-                    ...(this.ariaDescribedby
-                        ? { 'aria-describedby': this.ariaDescribedby }
+                    ...(this.iframeAriaDescribedby
+                        ? { 'aria-describedby': this.iframeAriaDescribedby }
                         : {})
                 }
-            }
-        };
+            };
+        }
     },
     methods: {
         onUpdate(value) {
@@ -169,3 +189,12 @@ export default {
     }
 };
 </script>
+
+<style lang="scss">
+    .tainacan-wysiwyg-keyboard-hint {
+        margin: 0.25rem 0 0;
+        color: var(--tainacan-gray4);
+        font-size: 0.75em;
+        line-height: 1.4;
+    }
+</style>
